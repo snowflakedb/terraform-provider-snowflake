8bcb53c0a0d4717d028f7a4f6582720f0110ff9f
Artur Sawicki <artur.sawicki@snowflake.com>
2025-04-25 13:40:30 +0200

    Prepare handling lists and sets in constructors (WIP)

--- pkg/acceptance/bettertestspoc/config/model/gen/model.go
+++ pkg/acceptance/bettertestspoc/config/model/gen/model.go
@@ -1,8 +1,10 @@
 package gen
 
 import (
+	"log"
 	"os"
 	"slices"
+	"strings"
 
 	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/internal/genhelpers"
 	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/resources"
@@ -34,6 +36,7 @@ type ResourceConfigBuilderAttributeModel struct {
 }
 
 func ModelFromResourceSchemaDetails(resourceSchemaDetails genhelpers.ResourceSchemaDetails) ResourceConfigBuilderModel {
+	additionalImports := []string{"encoding/json"}
 	attributes := make([]ResourceConfigBuilderAttributeModel, 0)
 	for _, attr := range resourceSchemaDetails.Attributes {
 		if slices.Contains([]string{resources.ShowOutputAttributeName, resources.ParametersAttributeName, resources.DescribeOutputAttributeName}, attr.Name) {
@@ -70,6 +73,14 @@ func ModelFromResourceSchemaDetails(resourceSchemaDetails genhelpers.ResourceSch
 		case schema.TypeString:
 			attributeType = "string"
 			variableMethod = "StringVariable"
+		case schema.TypeList, schema.TypeSet:
+			// We only run it for the required attributes because the `With` methods are not yet generated; we don't need to set the `variableMethod`.
+			// For now, the `With` method for complex object will still need to be added to _ext file.
+			if attr.Required {
+				attrType, additionalImport := handleAttributeTypeForListsAndSets(attr, resourceSchemaDetails.Name)
+				attributeType = attrType
+				additionalImports = append(additionalImports, additionalImport)
+			}
 		}
 
 		attributes = append(attributes, ResourceConfigBuilderAttributeModel{
@@ -88,7 +99,38 @@ func ModelFromResourceSchemaDetails(resourceSchemaDetails genhelpers.ResourceSch
 		Attributes: attributes,
 		PreambleModel: PreambleModel{
 			PackageName:               packageWithGenerateDirective,
-			AdditionalStandardImports: []string{"encoding/json"},
+			AdditionalStandardImports: additionalImports,
 		},
 	}
+}
+
+// handleAttributeTypeForListsAndSets handles model preparation for list and set attributes.
+// For simple types it's handled seamlessly.
+// For complex types, we need to define override in complexListAttributesOverrides.
+// Also, we need to import package (usually sdk) containing the type representing the given object.
+func handleAttributeTypeForListsAndSets(attr genhelpers.SchemaAttribute, resourceName string) (attributeType string, additionalImport string) {
+	switch attr.AttributeSubType {
+	case schema.TypeBool:
+		attributeType = "[]bool"
+	case schema.TypeInt:
+		attributeType = "[]int"
+	case schema.TypeFloat:
+		attributeType = "[]float"
+	case schema.TypeString:
+		attributeType = "[]string"
+	case schema.TypeMap:
+		if v, ok := complexListAttributesOverrides[resourceName]; ok {
+			if t, ok := v[attr.Name]; ok {
+				attributeType = "[]" + t
+				additionalImport = strings.Split(t, ".")[0]
+			} else {
+				log.Printf("[WARN] No complex list attribute override found for resource's %s attribute %s", resourceName, attr.Name)
+			}
+		} else {
+			log.Printf("[WARN] No complex list attribute overrides found for resource %s", resourceName)
+		}
+	default:
+		log.Printf("[WARN] Attribute's %s sub type could not be determined", attr.Name)
+	}
+	return
 }
