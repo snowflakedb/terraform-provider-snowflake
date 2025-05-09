827eac555852bbf4faa299b99895aac7af5cd53f
Henk Pors <hpors@anwb.nl>
2025-05-09 12:56:27 +0200

    Changes made as a reaction to PR comments. Not all comments are ticked off, further details requested.

--- pkg/datasources/tables.go
+++ pkg/datasources/tables.go
@@ -22,44 +22,10 @@ var tablesSchema = map[string]*schema.Schema{
 		Default:     true,
 		Description: "Runs DESC TABLE for each table returned by SHOW TABLES. The output of describe is saved to the description field. By default this value is set to true.",
 	},
-	"in": {
-		Type:        schema.TypeList,
-		Optional:    true,
-		Description: "IN clause to filter the list of tables",
-		MaxItems:    1,
-		Elem: &schema.Resource{
-			Schema: map[string]*schema.Schema{
-				"account": {
-					Type:         schema.TypeBool,
-					Optional:     true,
-					Description:  "Returns records for the entire account.",
-					ExactlyOneOf: []string{"in.0.account", "in.0.database", "in.0.schema"},
-				},
-				"database": {
-					Type:         schema.TypeString,
-					Optional:     true,
-					Description:  "Returns records for the current database in use or for a specified database.",
-					ExactlyOneOf: []string{"in.0.account", "in.0.database", "in.0.schema"},
-				},
-				"schema": {
-					Type:         schema.TypeString,
-					Optional:     true,
-					Description:  "Returns records for the current schema in use or a specified schema. Use fully qualified name.",
-					ExactlyOneOf: []string{"in.0.account", "in.0.database", "in.0.schema"},
-				},
-			},
-		},
-	},
-	"like": {
-		Type:        schema.TypeString,
-		Optional:    true,
-		Description: "Filters the output with **case-insensitive** pattern, with support for SQL wildcard characters (`%` and `_`).",
-	},
-	"starts_with": {
-		Type:        schema.TypeString,
-		Optional:    true,
-		Description: "Filters the output with **case-sensitive** characters indicating the beginning of the object name.",
-	},
+	"in":          extendedInSchema,
+	"like":        likeSchema,
+	"starts_with": startsWithSchema,
+	"limit":       limitFromSchema,
 	"tables": {
 		Type:        schema.TypeList,
 		Computed:    true,
@@ -99,37 +65,12 @@ func ReadTables(ctx context.Context, d *schema.ResourceData, meta any) diag.Diag
 	client := meta.(*provider.Context).Client
 	req := sdk.NewShowTableRequest()
 
-	if v, ok := d.GetOk("in"); ok {
-		in := v.([]any)[0].(map[string]any)
-		if v, ok := in["account"]; ok && v.(bool) {
-			req.WithIn(sdk.ExtendedIn{In: sdk.In{Account: sdk.Bool(true)}})
-		}
-		if v, ok := in["database"]; ok {
-			database := v.(string)
-			if database != "" {
-				req.WithIn(sdk.ExtendedIn{In: sdk.In{Database: sdk.NewAccountObjectIdentifier(database)}})
-			}
-		}
-		if v, ok := in["schema"]; ok {
-			schema := v.(string)
-			if schema != "" {
-				schemaId, err := sdk.ParseDatabaseObjectIdentifier(schema)
-				if err != nil {
-					return diag.FromErr(err)
-				}
-				req.WithIn(sdk.ExtendedIn{In: sdk.In{Schema: schemaId}})
-			}
-		}
-	}
-
-	if likePattern, ok := d.GetOk("like"); ok {
-		req.WithLike(sdk.Like{
-			Pattern: sdk.String(likePattern.(string)),
-		})
-	}
-
-	if v, ok := d.GetOk("starts_with"); ok {
-		req.WithStartsWith(v.(string))
+	handleLike(d, &req.Like)
+	handleStartsWith(d, &req.StartsWith)
+	handleLimitFrom(d, &req.Limit)
+	err := handleExtendedIn(d, &req.In)
+	if err != nil {
+		return diag.FromErr(err)
 	}
 
 	tables, err := client.Tables.Show(ctx, req)
