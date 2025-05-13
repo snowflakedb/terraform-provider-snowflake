b8d9403c1ea7bec79b5713d5fb5ea1e8f0633dfc
Henk Pors <hpors@anwb.nl>
2025-05-13 09:22:45 +0200

    fixed typo in pkg/datasources/tables.go and cleaned tables_acceptance_test.go based on given comments

--- pkg/datasources/tables_acceptance_test.go
+++ pkg/datasources/tables_acceptance_test.go
@@ -7,43 +7,49 @@ import (
 	"testing"
 
 	acc "github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/acceptance"
+
+	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/acceptance/testenvs"
 	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/sdk"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/hashicorp/terraform-plugin-testing/tfversion"
 )
 
 func TestAcc_Tables(t *testing.T) {
-	databaseName := acc.TestClient().Ids.Alpha()
-	schemaName := acc.TestClient().Ids.Alpha()
-	tableName := acc.TestClient().Ids.Alpha()
-	stageName := acc.TestClient().Ids.Alpha()
-	externalTableName := acc.TestClient().Ids.Alpha()
+	_ = testenvs.GetOrSkipTest(t, testenvs.EnableAcceptance)
+	acc.TestAccPreCheck(t)
+
+	schema, schemaCleanup := acc.TestClient().Schema.CreateSchema(t)
+	t.Cleanup(schemaCleanup)
+
+	tableId := acc.TestClient().Ids.RandomSchemaObjectIdentifierInSchema(schema.ID())
+	stageId := acc.TestClient().Ids.RandomSchemaObjectIdentifierInSchema(schema.ID())
+	externalTableId := acc.TestClient().Ids.RandomSchemaObjectIdentifierInSchema(schema.ID())
 
 	resource.Test(t, resource.TestCase{
 		ProtoV6ProviderFactories: acc.TestAccProtoV6ProviderFactories,
+		PreCheck:                 func() { acc.TestAccPreCheck(t) },
 		TerraformVersionChecks: []tfversion.TerraformVersionCheck{
 			tfversion.RequireAbove(tfversion.Version1_5_0),
 		},
 		CheckDestroy: nil,
 		Steps: []resource.TestStep{
 			{
-				Config: tables(databaseName, schemaName, tableName, stageName, externalTableName),
-				Check: resource.ComposeAggregateTestCheckFunc(
+				Config: tables(tableId, stageId, externalTableId),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("data.snowflake_tables.t", "database", tableId.DatabaseName()),
+					resource.TestCheckResourceAttr("data.snowflake_tables.t", "schema", tableId.SchemaName()),
+					resource.TestCheckResourceAttrSet("data.snowflake_tables.t", "tables.#"),
+					resource.TestCheckResourceAttr("data.snowflake_tables.t", "tables.#", "1"),
+					resource.TestCheckResourceAttr("data.snowflake_tables.t", "tables.0.name", tableId.Name()),
+					resource.TestCheckResourceAttr("data.snowflake_tables.in_schema", "database", tableId.DatabaseName()),
+					resource.TestCheckResourceAttr("data.snowflake_tables.in_schema", "schema", tableId.SchemaName()),
 					resource.TestCheckResourceAttr("data.snowflake_tables.in_schema", "tables.#", "1"),
-					resource.TestCheckResourceAttr("data.snowflake_tables.in_schema", "tables.0.name", tableName),
-					resource.TestCheckResourceAttrSet("data.snowflake_tables.in_schema", "tables.0.created_on"),
-					resource.TestCheckResourceAttr("data.snowflake_tables.in_schema", "tables.0.database_name", databaseName),
-					resource.TestCheckResourceAttr("data.snowflake_tables.in_schema", "tables.0.schema_name", schemaName),
-					resource.TestCheckResourceAttrSet("data.snowflake_tables.in_schema", "tables.0.owner"),
-					resource.TestCheckResourceAttr("data.snowflake_tables.in_schema", "tables.0.comment", ""),
-					resource.TestCheckResourceAttrSet("data.snowflake_tables.in_schema", "tables.0.text"),
-					resource.TestCheckResourceAttr("data.snowflake_tables.in_schema", "tables.0.is_secure", "false"),
-					resource.TestCheckResourceAttr("data.snowflake_tables.in_schema", "tables.0.is_materialized", "false"),
-					resource.TestCheckResourceAttr("data.snowflake_tables.in_schema", "tables.0.owner_role_type", "ROLE"),
-					resource.TestCheckResourceAttr("data.snowflake_tables.in_schema", "tables.0.change_tracking", "OFF"),
-
+					resource.TestCheckResourceAttr("data.snowflake_tables.in_schema", "tables.0.name", tableId.Name()),
+					resource.TestCheckResourceAttr("data.snowflake_tables.filtering", "database", tableId.DatabaseName()),
+					resource.TestCheckResourceAttr("data.snowflake_tables.filtering", "like", tableId.Name()),
 					resource.TestCheckResourceAttr("data.snowflake_tables.filtering", "tables.#", "1"),
-					resource.TestCheckResourceAttr("data.snowflake_tables.filtering", "tables.0.name", tableName),
+					resource.TestCheckResourceAttr("data.snowflake_tables.filtering", "tables.0.name", tableId.Name()),
+					resource.TestCheckResourceAttr("data.snowflake_tables.filtering", "tables.1.name", externalTableId.Name()),
 				),
 			},
 		},
@@ -77,7 +83,6 @@ func tables(tableId sdk.SchemaObjectIdentifier, stageId sdk.SchemaObjectIdentifi
 			name = "column1"
 			type = "STRING"
 			as = "TO_VARCHAR(TO_TIMESTAMP_NTZ(value:unix_timestamp_property::NUMBER, 3), 'yyyy-mm-dd-hh')"
-			as = "TO_VARCHAR(TO_TIMESTAMP_NTZ(value:unix_timestamp_property::NUMBER, 3), 'yyyy-mm-dd-hh')"
 		}
 	    file_format = "TYPE = CSV"
 	    location = "@${snowflake_stage.s.fully_qualified_name}"
@@ -86,16 +91,16 @@ func tables(tableId sdk.SchemaObjectIdentifier, stageId sdk.SchemaObjectIdentifi
 	data snowflake_tables "in_schema" {
 		depends_on = [snowflake_table.t, snowflake_external_table.et]
 		in {
-			schema = snowflake_schema.s.fully_qualified_name
+			schema = "%[2]s"
 		}
 	}
 
 	data snowflake_tables "filtering" {
 		depends_on = [snowflake_table.t, snowflake_external_table.et]
 		in {
-			database = snowflake_schema.s.database
+			database = "%[1]s"
 		}
-		like = "%[6]s"
+		like = "%[3]s"
 	}
-	`, databaseName, schemaName, tableName, stageName, externalTableName, tableName+"%", tableName+"%")
+	`, tableId.DatabaseName(), tableId.SchemaName(), tableId.Name(), stageId.Name(), externalTableId.Name())
 }
