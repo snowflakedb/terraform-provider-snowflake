fb806163d2de3b3e93253124103548c09e997f67
Artur Sawicki <artur.sawicki@snowflake.com>
2025-05-15 15:43:22 +0200

    Update TODO

--- pkg/testhelpers/helpers.go
+++ pkg/testhelpers/helpers.go
@@ -11,7 +11,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-// TODO [this PR]: move this file
+// TODO [next PRs]: move this file
 // TestFile creates a temporary file with the given filename, data and with the default permissions.
 // The directory is automatically removed when the test and all its subtests complete.
 // Each subsequent call to t.TempDir returns a unique directory.
