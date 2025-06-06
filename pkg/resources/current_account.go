efa634f2306e709e0673b9e13b96200e243c9fd5
Jan Cieślak <jan.cieslak@snowflake.com>
2025-06-06 10:08:46 +0200

    wip

--- pkg/resources/current_account.go
+++ pkg/resources/current_account.go
@@ -3,6 +3,7 @@ package resources
 import (
 	"context"
 	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/internal/collections"
+	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/internal/provider"
 	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/provider/resources"
 	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/sdk"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
@@ -13,29 +14,55 @@ var currentAccountSchema = map[string]*schema.Schema{
 	"resource_monitor": {
 		Type:             schema.TypeString,
 		Optional:         true,
-		Description:      relatedResourceDescription("Specifies an existing network policy. This network policy controls network traffic that is attempting to exchange an authorization code for an access or refresh token or to use a refresh token to obtain a new access token.", resources.NetworkPolicy),
+		Description:      "",
 		ValidateDiagFunc: IsValidIdentifier[sdk.AccountObjectIdentifier](),
 		DiffSuppressFunc: suppressIdentifierQuoting,
 	},
 	"authentication_policy": {
-		Type: schema.TypeString,
+		Type:             schema.TypeString,
+		Optional:         true,
+		Description:      "",
+		ValidateDiagFunc: IsValidIdentifier[sdk.SchemaObjectIdentifier](),
+		DiffSuppressFunc: suppressIdentifierQuoting,
 	},
 	"password_policy": {
-		Type: schema.TypeString,
+		Type:             schema.TypeString,
+		Optional:         true,
+		Description:      "",
+		ValidateDiagFunc: IsValidIdentifier[sdk.SchemaObjectIdentifier](),
+		DiffSuppressFunc: suppressIdentifierQuoting,
 	},
 	"session_policy": {
-		Type: schema.TypeString,
+		Type:             schema.TypeString,
+		Optional:         true,
+		Description:      "",
+		ValidateDiagFunc: IsValidIdentifier[sdk.SchemaObjectIdentifier](),
+		DiffSuppressFunc: suppressIdentifierQuoting,
 	},
 	"feature_policy": {
-		Type: schema.TypeString,
+		Type:             schema.TypeString,
+		Optional:         true,
+		Description:      "",
+		ValidateDiagFunc: IsValidIdentifier[sdk.SchemaObjectIdentifier](),
+		DiffSuppressFunc: suppressIdentifierQuoting,
 	},
 	"packages_policy": {
-		Type: schema.TypeString,
+		Type:             schema.TypeString,
+		Optional:         true,
+		Description:      "",
+		ValidateDiagFunc: IsValidIdentifier[sdk.SchemaObjectIdentifier](),
+		DiffSuppressFunc: suppressIdentifierQuoting,
 	},
-	"organization_user_group": {},
-	"tags": {
-		Type: schema.TypeString,
+	"organization_user_group": {
+		Type: schema.TypeSet,
+		Elem: &schema.Schema{
+			Type:             schema.TypeString,
+			ValidateDiagFunc: IsValidIdentifier[sdk.AccountObjectIdentifier](),
+		},
+		Optional:    true,
+		Description: "The list of organization user groups imported into the account.",
 	},
+	// TODO: Tags are done by tags_association resource
 }
 
 func CurrentAccount() *schema.Resource {
@@ -46,7 +73,7 @@ func CurrentAccount() *schema.Resource {
 		UpdateContext: TrackingUpdateWrapper(resources.Account, UpdateCurrentAccount),
 		DeleteContext: TrackingDeleteWrapper(resources.Account, DeleteCurrentAccount),
 
-		CustomizeDiff: TrackingCustomDiffWrapper(resources.Account, taskParametersCustomDiff),
+		CustomizeDiff: TrackingCustomDiffWrapper(resources.Account, accountParametersCustomDiff),
 
 		Schema: collections.MergeMaps(currentAccountSchema, accountParametersSchema),
 
@@ -55,19 +82,107 @@ func CurrentAccount() *schema.Resource {
 }
 
 func CreateCurrentAccount(ctx context.Context, d *schema.ResourceData, meta any) diag.Diagnostics {
-	return ReadCurrentAccount(false)(ctx, d, meta)
+	d.SetId("current_account")
+	return UpdateCurrentAccount(ctx, d, meta)
 }
 
 func ReadCurrentAccount(ctx context.Context, d *schema.ResourceData, meta any) diag.Diagnostics {
 	// TODO: Check if withExternalChangesMarking is needed here or if it can be removed
-	// client := meta.(*provider.Context).Client
+	client := meta.(*provider.Context).Client
+
+	// TODO: Get policy links
+
+	parameters, err := client.Accounts.ShowParameters(ctx)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	if err := handleAccountParameterRead(d, parameters); err != nil {
+		return diag.FromErr(err)
+	}
 
 	return nil
 }
 
 func UpdateCurrentAccount(ctx context.Context, d *schema.ResourceData, meta any) diag.Diagnostics {
-	// client := meta.(*provider.Context).Client
-	return ReadAccount(false)(ctx, d, meta)
+	client := meta.(*provider.Context).Client
+
+	if d.HasChange("resource_monitor") {
+		opts := new(sdk.AlterAccountOptions)
+		if err := accountObjectIdentifierAttributeUpdate(d, "resource_monitor", &opts.Set.ResourceMonitor, &opts.Unset.ResourceMonitor); err != nil {
+			return diag.FromErr(err)
+		}
+		if opts.Set.ResourceMonitor != nil || opts.Unset.ResourceMonitor != nil {
+			if err := client.Accounts.Alter(ctx, opts); err != nil {
+				return diag.FromErr(err)
+			}
+		}
+	}
+
+	if d.HasChange("authentication_policy") {
+		opts := new(sdk.AlterAccountOptions)
+		if err := schemaObjectIdentifierAttributeUpdate(d, "authentication_policy", &opts.Set.AuthenticationPolicy, &opts.Unset.AuthenticationPolicy); err != nil {
+			return diag.FromErr(err)
+		}
+		if opts.Set.AuthenticationPolicy != nil || opts.Unset.AuthenticationPolicy != nil {
+			if err := client.Accounts.Alter(ctx, opts); err != nil {
+				return diag.FromErr(err)
+			}
+		}
+	}
+
+	if d.HasChange("password_policy") {
+		opts := new(sdk.AlterAccountOptions)
+		if err := schemaObjectIdentifierAttributeUpdate(d, "password_policy", &opts.Set.PasswordPolicy, &opts.Unset.PasswordPolicy); err != nil {
+			return diag.FromErr(err)
+		}
+		if opts.Set.PasswordPolicy != nil || opts.Unset.PasswordPolicy != nil {
+			if err := client.Accounts.Alter(ctx, opts); err != nil {
+				return diag.FromErr(err)
+			}
+		}
+	}
+
+	if d.HasChange("session_policy") {
+		opts := new(sdk.AlterAccountOptions)
+		if err := schemaObjectIdentifierAttributeUpdate(d, "session_policy", &opts.Set.SessionPolicy, &opts.Unset.SessionPolicy); err != nil {
+			return diag.FromErr(err)
+		}
+		if opts.Set.SessionPolicy != nil || opts.Unset.SessionPolicy != nil {
+			if err := client.Accounts.Alter(ctx, opts); err != nil {
+				return diag.FromErr(err)
+			}
+		}
+	}
+
+	if d.HasChange("packages_policy") {
+		opts := new(sdk.AlterAccountOptions)
+		if err := schemaObjectIdentifierAttributeUpdate(d, "packages_policy", &opts.Set.PackagesPolicy, &opts.Unset.PackagesPolicy); err != nil {
+			return diag.FromErr(err)
+		}
+		if opts.Set.PackagesPolicy != nil || opts.Unset.PackagesPolicy != nil {
+			if err := client.Accounts.Alter(ctx, opts); err != nil {
+				return diag.FromErr(err)
+			}
+		}
+	}
+
+	//if errs := errors.Join(
+	//	alterAccountIfAttributeChanged(client, ctx, d, schemaObjectIdentifierAttributeUpdate, "feature_policy", func(opts *sdk.AlterAccountOptions) *sdk.SchemaObjectIdentifier { return opts.Set.FeaturePolicy }, func(opts *sdk.AlterAccountOptions) *bool { return opts.Unset.FeaturePolicy }),
+	//); errs != nil {
+	//	return diag.FromErr(errs)
+	//}
+
+	//before, after := d.GetChange("organization_user_group")
+	//// TODO: Update if not empty
+
+	setParameters := new(sdk.AccountSet)
+	unsetParameters := new(sdk.AccountUnset)
+	if diags := handleAccountParametersUpdate(d, setParameters, unsetParameters); diags != nil {
+		return diags
+	}
+
+	return ReadCurrentAccount(ctx, d, meta)
 }
 
 func DeleteCurrentAccount(_ context.Context, d *schema.ResourceData, _ any) diag.Diagnostics {
---
+++ pkg/testacc/resource_current_account_test.go
@@ -0,0 +1,38 @@
+//go:build !account_level_tests
+
+package testacc
+
+import (
+	"fmt"
+	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/acceptance/testenvs"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/tfversion"
+	"testing"
+)
+
+func TestAcc_CurrentAccount_Minimal(t *testing.T) {
+	_ = testenvs.GetOrSkipTest(t, testenvs.TestAccountCreate)
+
+	resourceMonitor, resourceMonitorCleanup := testClient().ResourceMonitor.CreateResourceMonitor(t)
+	t.Cleanup(resourceMonitorCleanup)
+
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: TestAccProtoV6ProviderFactories,
+		PreCheck:                 func() { TestAccPreCheck(t) },
+		TerraformVersionChecks: []tfversion.TerraformVersionCheck{
+			tfversion.RequireAbove(tfversion.Version1_5_0),
+		},
+		Steps: []resource.TestStep{
+			{
+				Config: fmt.Sprintf(`
+resource "snowflake_current_account" "current" {
+ resource_monitor = "%s"
+}
+`, resourceMonitor.ID().Name()),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("current_account.current", "resource_monitor", resourceMonitor.ID().Name()),
+				),
+			},
+		},
+	})
+}
