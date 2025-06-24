fe112a0ba211e43972e9d7721ab61de2ff4713f2
Artur Sawicki <artur.sawicki@snowflake.com>
2025-06-24 14:03:03 +0200

    Run plugin framework test

--- pkg/testacc/11_plugin_framework_poc_resource.go
+++ pkg/testacc/11_plugin_framework_poc_resource.go
@@ -3,8 +3,12 @@ package testacc
 import (
 	"context"
 
+	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/sdk"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/types"
 )
 
 func NewSomeResource() resource.Resource {
@@ -13,6 +17,11 @@ func NewSomeResource() resource.Resource {
 
 type SomeResource struct{}
 
+type someResourceModelV0 struct {
+	Name types.String `tfsdk:"name"`
+	Id   types.String `tfsdk:"id"`
+}
+
 func (r *SomeResource) Metadata(_ context.Context, request resource.MetadataRequest, response *resource.MetadataResponse) {
 	// TODO [mux-PR]: add method for this logic
 	response.TypeName = request.ProviderTypeName + "_some"
@@ -20,17 +29,36 @@ func (r *SomeResource) Metadata(_ context.Context, request resource.MetadataRequ
 
 func (r *SomeResource) Schema(_ context.Context, _ resource.SchemaRequest, response *resource.SchemaResponse) {
 	response.Schema = schema.Schema{
+		Version: 0,
 		Attributes: map[string]schema.Attribute{
-			"todo": schema.StringAttribute{
-				Description: "TODO",
-				Optional:    true,
+			"name": schema.StringAttribute{
+				Description: "Name for this example resource.",
+				Required:    true,
+			},
+			"id": schema.StringAttribute{
+				Computed:    true,
+				Description: "Identifier for this example resource.",
+				PlanModifiers: []planmodifier.String{
+					// TODO[mux-PR]: how it behaves with renames?
+					stringplanmodifier.UseStateForUnknown(),
+				},
 			},
 		},
 	}
 }
 
-func (r *SomeResource) Create(_ context.Context, _ resource.CreateRequest, _ *resource.CreateResponse) {
-	// TODO [mux-PR]: implement
+func (r *SomeResource) Create(ctx context.Context, request resource.CreateRequest, response *resource.CreateResponse) {
+	var data *someResourceModelV0
+	response.Diagnostics.Append(request.Plan.Get(ctx, &data)...)
+
+	name := data.Name.ValueString()
+	id := sdk.NewAccountObjectIdentifier(name)
+	data.Id = types.StringValue(id.FullyQualifiedName())
+
+	if response.Diagnostics.HasError() {
+		return
+	}
+	response.Diagnostics.Append(response.State.Set(ctx, &data)...)
 }
 
 func (r *SomeResource) Read(_ context.Context, _ resource.ReadRequest, _ *resource.ReadResponse) {
--- pkg/testacc/plugin_framework_poc_acceptance_test.go
+++ pkg/testacc/plugin_framework_poc_acceptance_test.go
@@ -4,11 +4,15 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/acceptance/bettertestspoc/assert"
+	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/sdk"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/hashicorp/terraform-plugin-testing/tfversion"
 )
 
 func TestAcc_TerraformPluginFrameworkPoc_InitialSetup(t *testing.T) {
+	id := testClient().Ids.RandomAccountObjectIdentifier()
+
 	resource.Test(t, resource.TestCase{
 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactoriesWithPluginPoc,
 		TerraformVersionChecks: []tfversion.TerraformVersionCheck{
@@ -20,17 +24,19 @@ func TestAcc_TerraformPluginFrameworkPoc_InitialSetup(t *testing.T) {
 		CheckDestroy: nil,
 		Steps: []resource.TestStep{
 			{
-				Config: someResourceConfig("abc"),
-				Check:  nil,
+				Config: someResourceConfig(id),
+				Check: assertThat(t,
+					assert.Check(resource.TestCheckResourceAttr("snowflake_some.test", "id", id.FullyQualifiedName())),
+				),
 			},
 		},
 	})
 }
 
-func someResourceConfig(value string) string {
+func someResourceConfig(id sdk.AccountObjectIdentifier) string {
 	return fmt.Sprintf(`
 resource "snowflake_some" "test" {
-  todo = "%s"
+  name = "%s"
 }
-`, value)
+`, id.Name())
 }
