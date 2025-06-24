9e1d7eca01ded8653beed24cb654159cc1543728
Jan Cieślak <jan.cieslak@snowflake.com>
2025-06-24 12:38:53 +0200

    wip

--- pkg/acceptance/helpers/test_client.go
+++ pkg/acceptance/helpers/test_client.go
@@ -41,7 +41,6 @@ type TestClient struct {
 	HybridTable                  *HybridTableClient
 	ImageRepository              *ImageRepositoryClient
 	InformationSchema            *InformationSchemaClient
-	Listing                      *ListingClient
 	MaskingPolicy                *MaskingPolicyClient
 	MaterializedView             *MaterializedViewClient
 	NetworkPolicy                *NetworkPolicyClient
@@ -124,7 +123,6 @@ func NewTestClient(c *sdk.Client, database string, schema string, warehouse stri
 		HybridTable:                  NewHybridTableClient(context, idsGenerator),
 		ImageRepository:              NewImageRepositoryClient(context, idsGenerator),
 		InformationSchema:            NewInformationSchemaClient(context, idsGenerator),
-		Listing:                      NewListingClient(context, idsGenerator),
 		MaskingPolicy:                NewMaskingPolicyClient(context, idsGenerator),
 		MaterializedView:             NewMaterializedViewClient(context, idsGenerator),
 		NetworkPolicy:                NewNetworkPolicyClient(context, idsGenerator),
