351fc62ff730ef1fd9af99f19affee773abdaf12
Jan Cieślak <jan.cieslak@snowflake.com>
2025-06-25 14:20:17 +0200

    Add organization account definition file

--- pkg/sdk/poc/main.go
+++ pkg/sdk/poc/main.go
@@ -53,6 +53,7 @@ var definitionMapping = map[string]*generator.Interface{
 	"compute_pools_def.go":                   sdk.ComputePoolsDef,
 	"git_repository_def.go":                  sdk.GitRepositoriesDef,
 	"services_def.go":                        sdk.ServicesDef,
+	"organization_accounts_def.go":           sdk.OrganizationAccountsDef,
 }
 
 func main() {
