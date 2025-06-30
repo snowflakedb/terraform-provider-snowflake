56053a67daac5f0bef4680f7551e8f0ecd333674
Jan Cieślak <jan.cieslak@snowflake.com>
2025-06-30 14:55:34 +0200

    add documentation for mapping helper functions

--- pkg/sdk/mapping_helpers.go
+++ pkg/sdk/mapping_helpers.go
@@ -11,6 +11,8 @@ func mapNullString(stringField **string, sqlValue sql.NullString) {
 	}
 }
 
+// mapNullStringWithMapping maps a sql.NullString to a pointer of type T using a provided mapper function.
+// Be careful with the sensitive values as the mapper function can return an error, which is then logged by this function.
 func mapNullStringWithMapping[T any](stringField **T, sqlValue sql.NullString, mapper func(string) (T, error)) {
 	if sqlValue.Valid {
 		if mappedValue, err := mapper(sqlValue.String); err == nil {
@@ -27,6 +29,8 @@ func mapNullBool(boolField **bool, sqlValue sql.NullBool) {
 	}
 }
 
+// mapStringWithMapping maps a string to a type T using a provided mapper function.
+// Be careful with the sensitive values as the mapper function can return an error, which is then logged by this function.
 func mapStringWithMapping[T any](stringField *T, sqlValue string, mapper func(string) (T, error)) {
 	if mappedValue, err := mapper(sqlValue); err == nil {
 		*stringField = mappedValue
