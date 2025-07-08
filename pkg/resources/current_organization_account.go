c8b8baf02368ed97657f9d70fc1c792df4939404
Jan Cieślak <jan.cieslak@snowflake.com>
2025-07-08 17:01:42 +0200

    chore: Implement Current Organization Account management and policies

--- pkg/resources/current_organization_account.go
+++ pkg/resources/current_organization_account.go
@@ -2,8 +2,10 @@ package resources
 
 import (
 	"context"
+	"strings"
 
 	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/internal/collections"
+	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/internal/provider"
 	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/provider/previewfeatures"
 	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/provider/resources"
 	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/sdk"
@@ -12,6 +14,8 @@ import (
 )
 
 var currentOrganizationAccountSchema = map[string]*schema.Schema{
+	// TODO: name will be also in the organization account resource, but it won't support alters and we have to decide if we want to support renames here or there.
+	// "name": { Type: schema.TypeString },
 	"resource_monitor": {
 		Type:             schema.TypeString,
 		Optional:         true,
@@ -55,17 +59,172 @@ func CurrentOrganizationAccount() *schema.Resource {
 }
 
 func CreateCurrentOrganizationAccount(ctx context.Context, d *schema.ResourceData, meta any) diag.Diagnostics {
-	return nil
+	client := meta.(*provider.Context).Client
+
+	if v, ok := d.GetOk("resource_monitor"); ok {
+		resourceMonitorId, err := sdk.ParseAccountObjectIdentifier(v.(string))
+		if err != nil {
+			return diag.FromErr(err)
+		}
+		if err := client.OrganizationAccounts.Alter(ctx, sdk.NewAlterOrganizationAccountRequest().WithSet(*sdk.NewOrganizationAccountSetRequest().WithResourceMonitor(resourceMonitorId))); err != nil {
+			return diag.FromErr(err)
+		}
+	} else {
+		if err := client.OrganizationAccounts.Alter(ctx, sdk.NewAlterOrganizationAccountRequest().WithUnset(*sdk.NewOrganizationAccountUnsetRequest().WithResourceMonitor(true))); err != nil {
+			return diag.FromErr(err)
+		}
+	}
+
+	if v, ok := d.GetOk("password_policy"); ok {
+		passwordPolicyId, err := sdk.ParseSchemaObjectIdentifier(v.(string))
+		if err != nil {
+			return diag.FromErr(err)
+		}
+		if err := client.OrganizationAccounts.Alter(ctx, sdk.NewAlterOrganizationAccountRequest().WithSet(*sdk.NewOrganizationAccountSetRequest().WithPasswordPolicy(passwordPolicyId))); err != nil {
+			return diag.FromErr(err)
+		}
+	} else {
+		if err := client.OrganizationAccounts.UnsetPolicySafely(ctx, sdk.PolicyKindPasswordPolicy); err != nil {
+			return diag.FromErr(err)
+		}
+	}
+
+	if v, ok := d.GetOk("session_policy"); ok {
+		sessionPolicyId, err := sdk.ParseSchemaObjectIdentifier(v.(string))
+		if err != nil {
+			return diag.FromErr(err)
+		}
+		if err := client.OrganizationAccounts.Alter(ctx, sdk.NewAlterOrganizationAccountRequest().WithSet(*sdk.NewOrganizationAccountSetRequest().WithSessionPolicy(sessionPolicyId))); err != nil {
+			return diag.FromErr(err)
+		}
+	} else {
+		if err := client.OrganizationAccounts.UnsetPolicySafely(ctx, sdk.PolicyKindSessionPolicy); err != nil {
+			return diag.FromErr(err)
+		}
+	}
+
+	setParameters := new(sdk.AccountParameters)
+	if diags := handleAccountParametersCreate(d, setParameters); diags != nil {
+		return diags
+	}
+	if *setParameters != (sdk.AccountParameters{}) {
+		if err := client.OrganizationAccounts.Alter(ctx, sdk.NewAlterOrganizationAccountRequest().WithSet(*sdk.NewOrganizationAccountSetRequest().WithParameters(*setParameters))); err != nil {
+			return diag.FromErr(err)
+		}
+	}
+
+	d.SetId("current_organization_account")
+
+	return ReadCurrentAccount(ctx, d, meta)
 }
 
 func ReadCurrentOrganizationAccount(ctx context.Context, d *schema.ResourceData, meta any) diag.Diagnostics {
+	client := meta.(*provider.Context).Client
+
+	// TODO: Is it domain account?
+	attachedPolicies, err := client.PolicyReferences.GetForEntity(ctx, sdk.NewGetForEntityPolicyReferenceRequest(sdk.NewAccountObjectIdentifier(client.GetAccountLocator()), sdk.PolicyEntityDomainAccount))
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	for _, policy := range attachedPolicies {
+		switch policy.PolicyKind {
+		case sdk.PolicyKindPasswordPolicy,
+			sdk.PolicyKindSessionPolicy:
+			if err := d.Set(strings.ToLower(string(policy.PolicyKind)), sdk.NewSchemaObjectIdentifier(*policy.PolicyDb, *policy.PolicySchema, policy.PolicyName).FullyQualifiedName()); err != nil {
+				return diag.FromErr(err)
+			}
+		}
+	}
+
+	parameters, err := client.OrganizationAccounts.ShowParameters(ctx)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	if err := handleAccountParameterRead(d, parameters); err != nil {
+		return diag.FromErr(err)
+	}
+
 	return nil
 }
 
 func UpdateCurrentOrganizationAccount(ctx context.Context, d *schema.ResourceData, meta any) diag.Diagnostics {
-	return nil
+	client := meta.(*provider.Context).Client
+
+	if d.HasChange("resource_monitor") {
+		if v, ok := d.GetOk("resource_monitor"); ok {
+			resourceMonitorId, err := sdk.ParseAccountObjectIdentifier(v.(string))
+			if err != nil {
+				return diag.FromErr(err)
+			}
+			if err := client.OrganizationAccounts.Alter(ctx, sdk.NewAlterOrganizationAccountRequest().WithSet(*sdk.NewOrganizationAccountSetRequest().WithResourceMonitor(resourceMonitorId))); err != nil {
+				return diag.FromErr(err)
+			}
+		} else {
+			if err := client.OrganizationAccounts.Alter(ctx, sdk.NewAlterOrganizationAccountRequest().WithUnset(*sdk.NewOrganizationAccountUnsetRequest().WithResourceMonitor(true))); err != nil {
+				return diag.FromErr(err)
+			}
+		}
+	}
+
+	if d.HasChange("password_policy") {
+		if v, ok := d.GetOk("password_policy"); ok {
+			passwordPolicyId, err := sdk.ParseSchemaObjectIdentifier(v.(string))
+			if err != nil {
+				return diag.FromErr(err)
+			}
+			if err := client.OrganizationAccounts.Alter(ctx, sdk.NewAlterOrganizationAccountRequest().WithSet(*sdk.NewOrganizationAccountSetRequest().WithPasswordPolicy(passwordPolicyId))); err != nil {
+				return diag.FromErr(err)
+			}
+		} else {
+			if err := client.OrganizationAccounts.UnsetPolicySafely(ctx, sdk.PolicyKindPasswordPolicy); err != nil {
+				return diag.FromErr(err)
+			}
+		}
+	}
+
+	if d.HasChange("session_policy") {
+		if v, ok := d.GetOk("session_policy"); ok {
+			sessionPolicyId, err := sdk.ParseSchemaObjectIdentifier(v.(string))
+			if err != nil {
+				return diag.FromErr(err)
+			}
+			if err := client.OrganizationAccounts.Alter(ctx, sdk.NewAlterOrganizationAccountRequest().WithSet(*sdk.NewOrganizationAccountSetRequest().WithSessionPolicy(sessionPolicyId))); err != nil {
+				return diag.FromErr(err)
+			}
+		} else {
+			if err := client.OrganizationAccounts.UnsetPolicySafely(ctx, sdk.PolicyKindSessionPolicy); err != nil {
+				return diag.FromErr(err)
+			}
+		}
+	}
+
+	setParameters := new(sdk.AccountParameters)
+	unsetParameters := new(sdk.AccountParametersUnset)
+	if diags := handleAccountParametersUpdate(d, setParameters, unsetParameters); diags != nil {
+		return diags
+	}
+	if *setParameters != (sdk.AccountParameters{}) {
+		if err := client.OrganizationAccounts.Alter(ctx, sdk.NewAlterOrganizationAccountRequest().WithSet(*sdk.NewOrganizationAccountSetRequest().WithParameters(*setParameters))); err != nil {
+			return diag.FromErr(err)
+		}
+	}
+	if *unsetParameters != (sdk.AccountParametersUnset{}) {
+		if err := client.OrganizationAccounts.Alter(ctx, sdk.NewAlterOrganizationAccountRequest().WithUnset(*sdk.NewOrganizationAccountUnsetRequest().WithParameters(*unsetParameters))); err != nil {
+			return diag.FromErr(err)
+		}
+	}
+
+	return ReadCurrentAccount(ctx, d, meta)
 }
 
 func DeleteCurrentOrganizationAccount(ctx context.Context, d *schema.ResourceData, meta any) diag.Diagnostics {
+	client := meta.(*provider.Context).Client
+	if err := client.OrganizationAccounts.UnsetAll(ctx); err != nil {
+		return diag.FromErr(err)
+	}
+
+	d.SetId("")
 	return nil
 }
