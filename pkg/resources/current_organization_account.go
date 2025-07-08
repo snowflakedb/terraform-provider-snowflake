23cf89ff7d6ff4ccb0a88466ccb8f48623828126
Jan Cieślak <jan.cieslak@snowflake.com>
2025-07-08 16:58:15 +0200

    chore: Add Current Organization Account resource and related assertions

--- pkg/resources/current_organization_account.go
+++ pkg/resources/current_organization_account.go
@@ -2,6 +2,7 @@ package resources
 
 import (
 	"context"
+
 	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/internal/collections"
 	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/provider/previewfeatures"
 	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/provider/resources"
