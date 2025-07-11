693109926703f86ec3aec1cbe4adc988f85c76d1
Jan Cieślak <jan.cieslak@snowflake.com>
2025-07-11 14:54:46 +0200

    chore: Update test commands to save results in JSON format and ensure unique file names

--- Makefile
+++ Makefile
@@ -73,20 +73,20 @@ create-test-output-directory:
 test-unit: create-test-output-directory ## run unit tests
 	go test -v -cover $$(go list ./... | grep -v -E "$(UNIT_TESTS_EXCLUDE_PATTERN)") -json | tee $$HOME/test_results/test_unit_output.json
 
-test-acceptance: ## run acceptance tests
-	TF_ACC=1 SF_TF_ACC_TEST_CONFIGURE_CLIENT_ONCE=true TEST_SF_TF_REQUIRE_TEST_OBJECT_SUFFIX=1 TEST_SF_TF_REQUIRE_GENERATED_RANDOM_VALUE=1 SF_TF_ACC_TEST_ENABLE_ALL_PREVIEW_FEATURES=true go test -run "^TestAcc_" -v -cover -timeout=120m ./pkg/testacc
+test-acceptance: create-test-output-directory ## run acceptance tests
+	TF_ACC=1 SF_TF_ACC_TEST_CONFIGURE_CLIENT_ONCE=true TEST_SF_TF_REQUIRE_TEST_OBJECT_SUFFIX=1 TEST_SF_TF_REQUIRE_GENERATED_RANDOM_VALUE=1 SF_TF_ACC_TEST_ENABLE_ALL_PREVIEW_FEATURES=true go test -run "^TestAcc_" -v -cover -timeout=120m ./pkg/testacc -json | tee $$HOME/test_results/test_acceptance_output.json
 
-test-account-level-features: ## run integration and acceptance test modifying account
-	TF_ACC=1 SF_TF_ACC_TEST_CONFIGURE_CLIENT_ONCE=true TEST_SF_TF_REQUIRE_TEST_OBJECT_SUFFIX=1 TEST_SF_TF_REQUIRE_GENERATED_RANDOM_VALUE=1 SF_TF_ACC_TEST_ENABLE_ALL_PREVIEW_FEATURES=true go test --tags=account_level_tests -run "^(TestAcc_|TestInt_)" -v -cover -timeout=30m ./pkg/testacc ./pkg/sdk/testint
+test-account-level-features: create-test-output-directory## run integration and acceptance test modifying account
+	TF_ACC=1 SF_TF_ACC_TEST_CONFIGURE_CLIENT_ONCE=true TEST_SF_TF_REQUIRE_TEST_OBJECT_SUFFIX=1 TEST_SF_TF_REQUIRE_GENERATED_RANDOM_VALUE=1 SF_TF_ACC_TEST_ENABLE_ALL_PREVIEW_FEATURES=true go test --tags=account_level_tests -run "^(TestAcc_|TestInt_)" -v -cover -timeout=30m ./pkg/testacc ./pkg/sdk/testint -json | tee $$HOME/test_results/test_account_level_output.json
 
 test-integration: create-test-output-directory ## run SDK integration tests
 	TEST_SF_TF_REQUIRE_TEST_OBJECT_SUFFIX=1 TEST_SF_TF_REQUIRE_GENERATED_RANDOM_VALUE=1 go test -run "^TestInt_" -v -cover -timeout=60m ./pkg/sdk/testint -json | tee $$HOME/test_results/test_integration_output.json
 
-test-functional: ## run functional tests of the underlying terraform libraries (currently SDKv2)
-	TF_ACC=1 TEST_SF_TF_ENABLE_OBJECT_RENAMING=1 go test -v -cover -timeout=10m ./pkg/testfunctional
+test-functional: create-test-output-directory ## run functional tests of the underlying terraform libraries (currently SDKv2)
+	TF_ACC=1 TEST_SF_TF_ENABLE_OBJECT_RENAMING=1 go test -v -cover -timeout=10m ./pkg/testfunctional -json | tee $$HOME/test_results/test_functional_output.json
 
-test-architecture: ## check architecture constraints between packages
-	go test ./pkg/architests/... -v
+test-architecture: create-test-output-directory ## check architecture constraints between packages
+	go test ./pkg/architests/... -v -json | tee $$HOME/test_results/test_architecture_output.json
 
 test-acceptance-%: ## run acceptance tests for the given resource only, e.g. test-acceptance-Warehouse
 	TF_ACC=1 TF_LOG=DEBUG SNOWFLAKE_DRIVER_TRACING=debug SF_TF_ACC_TEST_CONFIGURE_CLIENT_ONCE=true SF_TF_ACC_TEST_ENABLE_ALL_PREVIEW_FEATURES=true go test -run ^TestAcc_$*_ -v -timeout=20m ./pkg/testacc
--- pkg/scripts/save_test_results/save_test_results.go
+++ pkg/scripts/save_test_results/save_test_results.go
@@ -4,32 +4,32 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/acceptance/testenvs"
-	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/sdk"
 	"log"
 	"os"
-	"time"
+
+	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/sdk"
 )
 
 type TestType string
 
 const (
-	TestTypeUnit        TestType = "unit"
-	TestTypeIntegration TestType = "integration"
+	TestTypeUnit         TestType = "unit"
+	TestTypeIntegration  TestType = "integration"
+	TestTypeAcceptance   TestType = "acceptance"
+	TestTypeAccountLevel TestType = "account_level"
+	TestTypeFunctional   TestType = "functional"
+	TestTypeArchitecture TestType = "architecture"
 )
 
 var (
-	unitTestFileName        = "test_unit_output.json"
-	integrationTestFileName = "test_integration_output.json"
-
 	testResultsStageId = sdk.NewSchemaObjectIdentifier("TEST_RESULTS_DATABASE", "TEST_RESULTS_SCHEMA", "TEST_RESULTS_STAGE")
 	testResultsTableId = sdk.NewSchemaObjectIdentifier("TEST_RESULTS_DATABASE", "TEST_RESULTS_SCHEMA", "TEST_RESULTS_TABLE")
 )
 
 func main() {
-	testObjectSuffix, ok := os.LookupEnv(string(testenvs.TestObjectsSuffix))
+	testRunId, ok := os.LookupEnv("TEST_SF_TF_TEST_WORKFLOW_ID")
 	if !ok {
-		log.Fatal("Environment variable TEST_SF_TF_TEST_OBJECT_SUFFIX is not set")
+		log.Fatal("Environment variable TEST_SF_TF_TEST_WORKFLOW_ID is not set")
 	}
 
 	dirName, err := os.UserHomeDir()
@@ -38,16 +38,19 @@ func main() {
 	}
 	testResultsDirName := dirName + "/test_results"
 
-	client, err := sdk.NewDefaultClient()
+	clientConfig, err := sdk.ProfileConfig("fourth_test_account")
 	if err != nil {
-		log.Fatal("Failed to create SDK client:", err)
+		log.Fatal("Failed to get client config:", err)
 	}
 
-	testRunId := fmt.Sprintf("%s-%s", testObjectSuffix, time.Now().Format(time.RFC3339))
+	client, err := sdk.NewClient(clientConfig)
+	if err != nil {
+		log.Fatal("Failed to create a new client:", err)
+	}
 
 	if errs := errors.Join(
-		processTestResults(TestTypeUnit, testRunId, client, testResultsStageId, testResultsTableId, testResultsDirName, unitTestFileName),
-		processTestResults(TestTypeIntegration, testRunId, client, testResultsStageId, testResultsTableId, testResultsDirName, integrationTestFileName),
+		processTestResults(TestTypeUnit, testRunId, client, testResultsStageId, testResultsTableId, testResultsDirName),
+		//processTestResults(TestTypeIntegration, testRunId, client, testResultsStageId, testResultsTableId, testResultsDirName),
 	); errs != nil {
 		log.Fatal(errs)
 	}
@@ -64,13 +67,28 @@ delete from %s where
 	log.Println("Successfully processed test results")
 }
 
-func processTestResults(testType TestType, testRunId string, client *sdk.Client, testResultsStageId sdk.SchemaObjectIdentifier, testResultsTableId sdk.SchemaObjectIdentifier, testResultsDirName string, fileName string) error {
+func processTestResults(testType TestType, testRunId string, client *sdk.Client, testResultsStageId sdk.SchemaObjectIdentifier, testResultsTableId sdk.SchemaObjectIdentifier, testResultsDirName string) error {
+	fileName := fmt.Sprintf("test_%s_output.json", testType)
 	testResultsFilePath := testResultsDirName + "/" + fileName
 
-	// TODO: This shouldn't be overwritten, make sure that each test run has it's own id
-	if _, err := client.ExecUnsafe(context.Background(), fmt.Sprintf("put file://%s @%s auto_compress = true overwrite = true;", testResultsFilePath, testResultsStageId.FullyQualifiedName())); err != nil {
+	uniqueFileName := fmt.Sprintf("%s_test_%s_output.json", testRunId, testType)
+	uniqueTestResultsFilePath := testResultsDirName + "/" + uniqueFileName
+
+	// We have to rename them because it's not possible to pass different target file name in Snowflake,
+	// and we need to have unique file names for each test run (to avoid collisions with other test runs).
+	if err := os.Rename(testResultsFilePath, uniqueTestResultsFilePath); err != nil {
+		return fmt.Errorf("failed to rename test results file, err = %w", err)
+	}
+
+	if _, err := client.ExecUnsafe(context.Background(), fmt.Sprintf("put file://%s @%s auto_compress = true overwrite = true;", uniqueTestResultsFilePath, testResultsStageId.FullyQualifiedName())); err != nil {
 		return fmt.Errorf("failed to put test results file to stage, err = %w", err)
 	}
+	defer func() {
+		// Clean up the staged file after processing
+		if _, err := client.ExecUnsafe(context.Background(), fmt.Sprintf("delete @%s/%s;", testResultsStageId.FullyQualifiedName(), uniqueFileName)); err != nil {
+			log.Printf("failed to remove test results from stage, err = %v", err)
+		}
+	}()
 
 	if _, err := client.ExecUnsafe(context.Background(), fmt.Sprintf(`
 copy into %s(test_run_id, test_type, package_name, test_name, action, elapsed)
@@ -79,7 +97,7 @@ from (
 	from @%s/%s
 )
 on_error = 'continue';
-`, testResultsTableId.FullyQualifiedName(), testRunId, testType, testResultsStageId.FullyQualifiedName(), fileName)); err != nil {
+`, testResultsTableId.FullyQualifiedName(), testRunId, testType, testResultsStageId.FullyQualifiedName(), uniqueFileName)); err != nil {
 		return fmt.Errorf("failed to put test results file to stage, err = %w", err)
 	}
 
