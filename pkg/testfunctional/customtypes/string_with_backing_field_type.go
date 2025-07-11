9c2a32188cf1c4397e1f604bc9a5610d112515f6
Artur Sawicki <artur.sawicki@snowflake.com>
2025-07-11 14:40:27 +0200

    Prepare the string base type

---
+++ pkg/testfunctional/customtypes/string_with_backing_field_type.go
@@ -0,0 +1,66 @@
+package customtypes
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-framework/attr"
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/types/basetypes"
+	"github.com/hashicorp/terraform-plugin-go/tftypes"
+)
+
+type StringWithBackingFieldType struct {
+	basetypes.StringType
+}
+
+// String returns a human-readable string of the type name.
+func (t StringWithBackingFieldType) String() string {
+	return "customtypes.StringWithBackingFieldType"
+}
+
+func (t StringWithBackingFieldType) ValueType(_ context.Context) attr.Value {
+	return StringWithBackingFieldValue{}
+}
+
+// Equal returns true if the given type is equivalent.
+// TODO [mux-PRs]: adjust
+func (t StringWithBackingFieldType) Equal(o attr.Type) bool {
+	other, ok := o.(StringWithBackingFieldType)
+
+	if !ok {
+		return false
+	}
+
+	return t.StringType.Equal(other.StringType)
+}
+
+// ValueFromString returns a StringValuable type given a StringValue.
+// TODO [mux-PRs]: adjust
+func (t StringWithBackingFieldType) ValueFromString(_ context.Context, in basetypes.StringValue) (basetypes.StringValuable, diag.Diagnostics) {
+	return StringWithBackingFieldValue{
+		StringValue: in,
+	}, nil
+}
+
+// ValueFromTerraform returns a Value given a tftypes.Value. This is meant to convert the tftypes.Value into a more convenient Go type
+// for the provider to consume the data with.
+// TODO [mux-PRs]: adjust
+func (t StringWithBackingFieldType) ValueFromTerraform(ctx context.Context, in tftypes.Value) (attr.Value, error) {
+	attrValue, err := t.StringType.ValueFromTerraform(ctx, in)
+	if err != nil {
+		return nil, err
+	}
+
+	stringValue, ok := attrValue.(basetypes.StringValue)
+	if !ok {
+		return nil, fmt.Errorf("unexpected value type of %T", attrValue)
+	}
+
+	stringValuable, diags := t.ValueFromString(ctx, stringValue)
+	if diags.HasError() {
+		return nil, fmt.Errorf("unexpected error converting StringValue to StringValuable: %v", diags)
+	}
+
+	return stringValuable, nil
+}
---
+++ pkg/testfunctional/customtypes/string_with_backing_field_value.go
@@ -0,0 +1,80 @@
+package customtypes
+
+import (
+	"context"
+	"encoding/json"
+
+	"github.com/hashicorp/terraform-plugin-framework/attr"
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/types/basetypes"
+)
+
+type StringWithBackingFieldValue struct {
+	basetypes.StringValue
+}
+
+func (v StringWithBackingFieldValue) Type(_ context.Context) attr.Type {
+	return StringWithBackingFieldType{}
+}
+
+// Equal returns true if the given value is equivalent.
+// TODO [mux-PRs]: adjust
+func (v StringWithBackingFieldValue) Equal(o attr.Value) bool {
+	other, ok := o.(StringWithBackingFieldValue)
+
+	if !ok {
+		return false
+	}
+
+	return v.StringValue.Equal(other.StringValue)
+}
+
+// TODO [mux-PRs]: adjust
+func (v StringWithBackingFieldValue) Unmarshal(target any) diag.Diagnostics {
+	diags := diag.Diagnostics{}
+
+	if v.IsNull() {
+		diags.Append(diag.NewErrorDiagnostic("String with backing field Unmarshal Error", "string value is null"))
+		return diags
+	}
+
+	if v.IsUnknown() {
+		diags.Append(diag.NewErrorDiagnostic("String with backing field Unmarshal Error", "string value is unknown"))
+		return diags
+	}
+
+	err := json.Unmarshal([]byte(v.ValueString()), target)
+	if err != nil {
+		diags.Append(diag.NewErrorDiagnostic("String with backing field Unmarshal Error", err.Error()))
+	}
+
+	return diags
+}
+
+// NewStringWithBackingFieldValueNull creates a StringWithBackingFieldValue with a null value. Determine whether the value is null via IsNull method.
+func NewStringWithBackingFieldValueNull() StringWithBackingFieldValue {
+	return StringWithBackingFieldValue{
+		StringValue: basetypes.NewStringNull(),
+	}
+}
+
+// NewStringWithBackingFieldValueUnknown creates a StringWithBackingFieldValue with an unknown value. Determine whether the value is unknown via IsUnknown method.
+func NewStringWithBackingFieldValueUnknown() StringWithBackingFieldValue {
+	return StringWithBackingFieldValue{
+		StringValue: basetypes.NewStringUnknown(),
+	}
+}
+
+// NewStringWithBackingFieldValueValue creates a StringWithBackingFieldValue with a known value. Access the value via ValueString method.
+func NewStringWithBackingFieldValueValue(value string) StringWithBackingFieldValue {
+	return StringWithBackingFieldValue{
+		StringValue: basetypes.NewStringValue(value),
+	}
+}
+
+// NewStringWithBackingFieldValuePointerValue creates a StringWithBackingFieldValue with a null value if nil or a known value. Access the value via ValueStringPointer method.
+func NewStringWithBackingFieldValuePointerValue(value *string) StringWithBackingFieldValue {
+	return StringWithBackingFieldValue{
+		StringValue: basetypes.NewStringPointerValue(value),
+	}
+}
