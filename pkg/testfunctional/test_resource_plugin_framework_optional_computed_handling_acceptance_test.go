399c30c98d2755b04e977d606da2470d59816148
Artur Sawicki <artur.sawicki@snowflake.com>
2025-07-18 10:18:48 +0200

    Fix after review

--- pkg/testfunctional/test_resource_plugin_framework_optional_computed_handling_acceptance_test.go
+++ pkg/testfunctional/test_resource_plugin_framework_optional_computed_handling_acceptance_test.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"testing"
 
+	tfjson "github.com/hashicorp/terraform-json"
+
+	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/acceptance/planchecks"
 	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/sdk"
 	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/testfunctional"
 	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/testfunctional/common"
@@ -12,9 +15,11 @@ import (
 	"github.com/hashicorp/terraform-plugin-testing/tfversion"
 )
 
+const optionalComputedHandlingPrivateDefaultValue = "default value - optional computed handling"
+
 var optionalComputedHandler = common.NewDynamicHandlerWithDefaultValueAndReplaceWithFunc[testfunctional.OptionalComputedOpts](
 	testfunctional.OptionalComputedOpts{
-		StringValue: sdk.Pointer("default text"),
+		StringValue: sdk.Pointer(optionalComputedHandlingPrivateDefaultValue),
 	}, optionalComputedOptsReplaceWithNonNil,
 )
 
@@ -36,6 +41,8 @@ func TestAcc_TerraformPluginFrameworkFunctional_OptionalComputed(t *testing.T) {
 	resourceType := fmt.Sprintf("%s_optional_computed", PluginFrameworkFunctionalTestsProviderName)
 	resourceReference := fmt.Sprintf("%s.test", resourceType)
 
+	value := "some value"
+
 	resource.Test(t, resource.TestCase{
 		ProtoV6ProviderFactories: providerForPluginFrameworkFunctionalTestsFactories,
 		TerraformVersionChecks: []tfversion.TerraformVersionCheck{
@@ -52,7 +59,7 @@ func TestAcc_TerraformPluginFrameworkFunctional_OptionalComputed(t *testing.T) {
 				Config: optionalComputedAllSetConfig(id, resourceType),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr(resourceReference, "id", id.FullyQualifiedName()),
-					resource.TestCheckResourceAttr(resourceReference, "string_value", "some value"),
+					resource.TestCheckResourceAttr(resourceReference, "string_value", value),
 				),
 			},
 			// remove all from config (to validate that unset is run correctly and default value is in state)
@@ -60,12 +67,37 @@ func TestAcc_TerraformPluginFrameworkFunctional_OptionalComputed(t *testing.T) {
 				ConfigPlanChecks: resource.ConfigPlanChecks{
 					PreApply: []plancheck.PlanCheck{
 						plancheck.ExpectResourceAction(resourceReference, plancheck.ResourceActionUpdate),
+						planchecks.ExpectChange(resourceReference, "string_value", tfjson.ActionUpdate, &value, nil),
+					},
+					PostApplyPostRefresh: []plancheck.PlanCheck{
+						plancheck.ExpectResourceAction(resourceReference, plancheck.ResourceActionUpdate),
+						planchecks.ExpectChange(resourceReference, "string_value", tfjson.ActionUpdate, sdk.Pointer(optionalComputedHandlingPrivateDefaultValue), nil),
 					},
 				},
 				Config: optionalComputedNoneSetConfig(id, resourceType),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr(resourceReference, "id", id.FullyQualifiedName()),
-					resource.TestCheckResourceAttr(resourceReference, "string_value", "default text"),
+					resource.TestCheckResourceAttr(resourceReference, "string_value", optionalComputedHandlingPrivateDefaultValue),
+				),
+				// Because our plan modifier react to the situation config == null and state != null, it will be a perma diff.
+				ExpectNonEmptyPlan: true,
+			},
+			// Adding the same step to show it ends the same way (hence - perma diff)
+			{
+				ConfigPlanChecks: resource.ConfigPlanChecks{
+					PreApply: []plancheck.PlanCheck{
+						plancheck.ExpectResourceAction(resourceReference, plancheck.ResourceActionUpdate),
+						planchecks.ExpectChange(resourceReference, "string_value", tfjson.ActionUpdate, sdk.Pointer(optionalComputedHandlingPrivateDefaultValue), nil),
+					},
+					PostApplyPostRefresh: []plancheck.PlanCheck{
+						plancheck.ExpectResourceAction(resourceReference, plancheck.ResourceActionUpdate),
+						planchecks.ExpectChange(resourceReference, "string_value", tfjson.ActionUpdate, sdk.Pointer(optionalComputedHandlingPrivateDefaultValue), nil),
+					},
+				},
+				Config: optionalComputedNoneSetConfig(id, resourceType),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceReference, "id", id.FullyQualifiedName()),
+					resource.TestCheckResourceAttr(resourceReference, "string_value", optionalComputedHandlingPrivateDefaultValue),
 				),
 				// Because our plan modifier react to the situation config == null and state != null, it will be a perma diff.
 				ExpectNonEmptyPlan: true,
