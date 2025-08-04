a2c04ba9240947d380738ca561e6d967aa371c88
Jan Cieślak <jan.cieslak@snowflake.com>
2025-08-04 16:45:25 +0200

    Changes after review

--- pkg/sdk/sweepers.go
+++ pkg/sdk/sweepers.go
@@ -2,10 +2,12 @@ package sdk
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"log"
 	"slices"
 	"strings"
+	"time"
 )
 
 func SweepAfterIntegrationTests(client *Client, suffix string) error {
@@ -32,7 +34,7 @@ func sweep(client *Client, suffix string) error {
 		getAccountPolicyAttachmentsSweeper(client),
 		getResourceMonitorSweeper(client, suffix),
 		getNetworkPolicySweeper(client, suffix),
-		getUserSweeper(client, suffix),
+		nukeUsers(client, suffix),
 		getFailoverGroupSweeper(client, suffix),
 		getShareSweeper(client, suffix),
 		getDatabaseSweeper(client, suffix),
@@ -101,48 +103,6 @@ func getNetworkPolicySweeper(client *Client, suffix string) func() error {
 				}
 			} else {
 				log.Printf("[DEBUG] Skipping network policy %s", np.ID().FullyQualifiedName())
-			}
-		}
-
-		return nil
-	}
-}
-
-// getUserSweeper was introduced to make sure that users created during tests are cleaned up.
-// It's required as users that have connections to policies within databases, block deletion of the whole database.
-// In Snowflake, the users can be removed without prior unsetting policies, but policies cannot be removed without firstly unlinking them from other objects.
-func getUserSweeper(client *Client, suffix string) func() error {
-	protectedUsers := []string{
-		"SNOWFLAKE",
-		"ARTUR_SAWICKI",
-		"ARTUR_SAWICKI_LEGACY",
-		"JAKUB_MICHALAK",
-		"JAKUB_MICHALAK_LEGACY",
-		"JAN_CIESLAK",
-		"JAN_CIESLAK_LEGACY",
-		"TERRAFORM_SVC_ACCOUNT",
-		"TEST_CI_SERVICE_USER",
-		"PENTESTING_USER_1",
-		"PENTESTING_USER_2",
-	}
-
-	return func() error {
-		log.Printf("[DEBUG] Sweeping users with suffix %s", suffix)
-		ctx := context.Background()
-
-		urs, err := client.Users.Show(ctx, new(ShowUserOptions))
-		if err != nil {
-			return fmt.Errorf("SHOW USERS ended with error, err = %w", err)
-		}
-
-		for _, u := range urs {
-			if strings.HasSuffix(u.Name, suffix) && !slices.Contains(protectedUsers, u.ID().Name()) {
-				log.Printf("[DEBUG] Dropping user %s", u.ID().FullyQualifiedName())
-				if err := client.Users.Drop(ctx, u.ID(), &DropUserOptions{IfExists: Bool(true)}); err != nil {
-					return fmt.Errorf("DROP USER for %s, ended with error, err = %w", u.ID().FullyQualifiedName(), err)
-				}
-			} else {
-				log.Printf("[DEBUG] Skipping user %s", u.ID().FullyQualifiedName())
 			}
 		}
 
@@ -273,5 +233,174 @@ func getRoleSweeper(client *Client, suffix string) func() error {
 			}
 		}
 		return nil
+	}
+}
+
+// TODO [SNOW-867247]: generalize nuke methods (sweepers too)
+// TODO [SNOW-1658402]: handle the ownership problem while handling the better role setup for tests
+func nukeWarehouses(client *Client, prefix string) func() error {
+	protectedWarehouses := []string{
+		"SNOWFLAKE",
+		"SYSTEM$STREAMLIT_NOTEBOOK_WH",
+	}
+
+	return func() error {
+		log.Printf("[DEBUG] Nuking warehouses with prefix %s", prefix)
+		ctx := context.Background()
+
+		var like *Like = nil
+		if prefix != "" {
+			like = &Like{Pattern: String(prefix)}
+		}
+
+		whs, err := client.Warehouses.Show(ctx, &ShowWarehouseOptions{Like: like})
+		if err != nil {
+			return fmt.Errorf("sweeping warehouses ended with error, err = %w", err)
+		}
+		var errs []error
+		log.Printf("[DEBUG] Found %d warehouses matching search criteria", len(whs))
+		for idx, wh := range whs {
+			log.Printf("[DEBUG] Processing warehouse [%d/%d]: %s...", idx+1, len(whs), wh.ID().FullyQualifiedName())
+			if !slices.Contains(protectedWarehouses, wh.Name) && wh.CreatedOn.Before(time.Now().Add(-8*time.Hour)) {
+				if wh.Owner != "ACCOUNTADMIN" {
+					log.Printf("[DEBUG] Granting ownership on warehouse %s, to ACCOUNTADMIN", wh.ID().FullyQualifiedName())
+					err := client.Grants.GrantOwnership(
+						ctx,
+						OwnershipGrantOn{Object: &Object{
+							ObjectType: ObjectTypeWarehouse,
+							Name:       wh.ID(),
+						}},
+						OwnershipGrantTo{
+							AccountRoleName: Pointer(NewAccountObjectIdentifier("ACCOUNTADMIN")),
+						},
+						nil,
+					)
+					if err != nil {
+						errs = append(errs, fmt.Errorf("granting ownership on warehouse %s ended with error, err = %w", wh.ID().FullyQualifiedName(), err))
+						continue
+					}
+				}
+
+				log.Printf("[DEBUG] Dropping warehouse %s, created at: %s", wh.ID().FullyQualifiedName(), wh.CreatedOn.String())
+				if err := client.Warehouses.Drop(ctx, wh.ID(), &DropWarehouseOptions{IfExists: Bool(true)}); err != nil {
+					log.Printf("[DEBUG] Dropping warehouse %s, resulted in error %v", wh.ID().FullyQualifiedName(), err)
+					errs = append(errs, fmt.Errorf("sweeping warehouse %s ended with error, err = %w", wh.ID().FullyQualifiedName(), err))
+				}
+			} else {
+				log.Printf("[DEBUG] Skipping warehouse %s, created at: %s", wh.ID().FullyQualifiedName(), wh.CreatedOn.String())
+			}
+		}
+		return errors.Join(errs...)
+	}
+}
+
+func nukeDatabases(client *Client, prefix string) func() error {
+	protectedDatabases := []string{
+		"SNOWFLAKE",
+		"MFA_ENFORCEMENT_POLICY",
+	}
+
+	return func() error {
+		log.Printf("[DEBUG] Nuking databases with prefix %s", prefix)
+		ctx := context.Background()
+
+		var like *Like = nil
+		if prefix != "" {
+			like = &Like{Pattern: String(prefix)}
+		}
+		dbs, err := client.Databases.Show(ctx, &ShowDatabasesOptions{Like: like})
+		if err != nil {
+			return fmt.Errorf("sweeping databases ended with error, err = %w", err)
+		}
+		var errs []error
+		log.Printf("[DEBUG] Found %d databases matching search criteria", len(dbs))
+		for idx, db := range dbs {
+			if db.Owner != "ACCOUNTADMIN" {
+				log.Printf("[DEBUG] Granting ownership on database %s, to ACCOUNTADMIN", db.ID().FullyQualifiedName())
+				err := client.Grants.GrantOwnership(
+					ctx,
+					OwnershipGrantOn{Object: &Object{
+						ObjectType: ObjectTypeDatabase,
+						Name:       db.ID(),
+					}},
+					OwnershipGrantTo{
+						AccountRoleName: Pointer(NewAccountObjectIdentifier("ACCOUNTADMIN")),
+					},
+					nil,
+				)
+				if err != nil {
+					errs = append(errs, fmt.Errorf("granting ownership on database %s ended with error, err = %w", db.ID().FullyQualifiedName(), err))
+					continue
+				}
+			}
+
+			log.Printf("[DEBUG] Processing database [%d/%d]: %s...", idx+1, len(dbs), db.ID().FullyQualifiedName())
+			if !slices.Contains(protectedDatabases, db.Name) && db.CreatedOn.Before(time.Now().Add(-8*time.Hour)) {
+				log.Printf("[DEBUG] Dropping database %s, created at: %s", db.ID().FullyQualifiedName(), db.CreatedOn.String())
+				if err := client.Databases.Drop(ctx, db.ID(), &DropDatabaseOptions{IfExists: Bool(true)}); err != nil {
+					log.Printf("[DEBUG] Dropping database %s, resulted in error %v", db.ID().FullyQualifiedName(), err)
+					errs = append(errs, fmt.Errorf("sweeping database %s ended with error, err = %w", db.ID().FullyQualifiedName(), err))
+				}
+			} else {
+				log.Printf("[DEBUG] Skipping database %s, created at: %s", db.ID().FullyQualifiedName(), db.CreatedOn.String())
+			}
+		}
+		return errors.Join(errs...)
+	}
+}
+
+func nukeUsers(client *Client, suffix string) func() error {
+	protectedUsers := []string{
+		"SNOWFLAKE",
+		"ARTUR_SAWICKI",
+		"ARTUR_SAWICKI_LEGACY",
+		"JAKUB_MICHALAK",
+		"JAKUB_MICHALAK_LEGACY",
+		"JAN_CIESLAK",
+		"JAN_CIESLAK_LEGACY",
+		"TERRAFORM_SVC_ACCOUNT",
+		"TEST_CI_SERVICE_USER",
+		"PENTESTING_USER_1",
+		"PENTESTING_USER_2",
+	}
+
+	return func() error {
+		ctx := context.Background()
+
+		var userDropCondition func(u User) bool
+		if suffix != "" {
+			log.Printf("[DEBUG] Sweeping users with suffix %s", suffix)
+			userDropCondition = func(u User) bool {
+				return strings.HasSuffix(u.Name, suffix) && !slices.Contains(protectedUsers, u.ID().Name())
+			}
+		} else {
+			log.Println("[DEBUG] Sweeping stale users")
+			userDropCondition = func(u User) bool {
+				return !slices.Contains(protectedUsers, u.Name) && u.CreatedOn.Before(time.Now().Add(-15*time.Minute))
+			}
+		}
+
+		urs, err := client.Users.Show(ctx, new(ShowUserOptions))
+		if err != nil {
+			return fmt.Errorf("SHOW USERS ended with error, err = %w", err)
+		}
+
+		log.Printf("[DEBUG] Found %d users", len(urs))
+
+		var errs []error
+		for idx, user := range urs {
+			log.Printf("[DEBUG] Processing user [%d/%d]: %s...", idx+1, len(urs), user.ID().FullyQualifiedName())
+
+			if userDropCondition(user) {
+				log.Printf("[DEBUG] Dropping user %s", user.ID().FullyQualifiedName())
+				if err := client.Users.Drop(ctx, user.ID(), &DropUserOptions{IfExists: Bool(true)}); err != nil {
+					errs = append(errs, fmt.Errorf("sweeping user %s ended with error, err = %w", user.ID().FullyQualifiedName(), err))
+				}
+			} else {
+				log.Printf("[DEBUG] Skipping user %s", user.ID().FullyQualifiedName())
+			}
+		}
+
+		return errors.Join(errs...)
 	}
 }
--- pkg/sdk/sweepers_test.go
+++ pkg/sdk/sweepers_test.go
@@ -1,18 +1,12 @@
 package sdk
 
 import (
-	"context"
-	"errors"
 	"fmt"
-	"log"
-	"slices"
-	"testing"
-	"time"
-
 	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/acceptance/helpers/random/acceptancetests"
 	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/acceptance/helpers/random/integrationtests"
 	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/acceptance/testenvs"
 	"github.com/stretchr/testify/assert"
+	"testing"
 )
 
 // TODO [SNOW-867247]: move the sweepers outside of the sdk package
@@ -81,7 +75,7 @@ func Test_Sweeper_NukeStaleObjects(t *testing.T) {
 
 	t.Run("sweep users", func(t *testing.T) {
 		for _, c := range allClients {
-			err := nukeUsers(c)()
+			err := nukeUsers(c, "")()
 			assert.NoError(t, err)
 		}
 	})
@@ -108,158 +102,4 @@ func Test_Sweeper_NukeStaleObjects(t *testing.T) {
 
 	// TODO [SNOW-867247]: nuke external oauth integrations because of errors like
 	// Error: 003524 (22023): SQL execution error: An integration with the given issuer already exists for this account
-}
-
-// TODO [SNOW-867247]: generalize nuke methods (sweepers too)
-// TODO [SNOW-1658402]: handle the ownership problem while handling the better role setup for tests
-func nukeWarehouses(client *Client, prefix string) func() error {
-	protectedWarehouses := []string{
-		"SNOWFLAKE",
-		"SYSTEM$STREAMLIT_NOTEBOOK_WH",
-	}
-
-	return func() error {
-		log.Printf("[DEBUG] Nuking warehouses with prefix %s", prefix)
-		ctx := context.Background()
-
-		var like *Like = nil
-		if prefix != "" {
-			like = &Like{Pattern: String(prefix)}
-		}
-
-		whs, err := client.Warehouses.Show(ctx, &ShowWarehouseOptions{Like: like})
-		if err != nil {
-			return fmt.Errorf("sweeping warehouses ended with error, err = %w", err)
-		}
-		var errs []error
-		log.Printf("[DEBUG] Found %d warehouses matching search criteria", len(whs))
-		for idx, wh := range whs {
-			log.Printf("[DEBUG] Processing warehouse [%d/%d]: %s...", idx+1, len(whs), wh.ID().FullyQualifiedName())
-			if !slices.Contains(protectedWarehouses, wh.Name) && wh.CreatedOn.Before(time.Now().Add(-8*time.Hour)) {
-				if wh.Owner != "ACCOUNTADMIN" {
-					log.Printf("[DEBUG] Granting ownership on warehouse %s, to ACCOUNTADMIN", wh.ID().FullyQualifiedName())
-					err := client.Grants.GrantOwnership(
-						ctx,
-						OwnershipGrantOn{Object: &Object{
-							ObjectType: ObjectTypeWarehouse,
-							Name:       wh.ID(),
-						}},
-						OwnershipGrantTo{
-							AccountRoleName: Pointer(NewAccountObjectIdentifier("ACCOUNTADMIN")),
-						},
-						nil,
-					)
-					if err != nil {
-						errs = append(errs, fmt.Errorf("granting ownership on warehouse %s ended with error, err = %w", wh.ID().FullyQualifiedName(), err))
-						continue
-					}
-				}
-
-				log.Printf("[DEBUG] Dropping warehouse %s, created at: %s", wh.ID().FullyQualifiedName(), wh.CreatedOn.String())
-				if err := client.Warehouses.Drop(ctx, wh.ID(), &DropWarehouseOptions{IfExists: Bool(true)}); err != nil {
-					log.Printf("[DEBUG] Dropping warehouse %s, resulted in error %v", wh.ID().FullyQualifiedName(), err)
-					errs = append(errs, fmt.Errorf("sweeping warehouse %s ended with error, err = %w", wh.ID().FullyQualifiedName(), err))
-				}
-			} else {
-				log.Printf("[DEBUG] Skipping warehouse %s, created at: %s", wh.ID().FullyQualifiedName(), wh.CreatedOn.String())
-			}
-		}
-		return errors.Join(errs...)
-	}
-}
-
-func nukeDatabases(client *Client, prefix string) func() error {
-	protectedDatabases := []string{
-		"SNOWFLAKE",
-		"MFA_ENFORCEMENT_POLICY",
-	}
-
-	return func() error {
-		log.Printf("[DEBUG] Nuking databases with prefix %s", prefix)
-		ctx := context.Background()
-
-		var like *Like = nil
-		if prefix != "" {
-			like = &Like{Pattern: String(prefix)}
-		}
-		dbs, err := client.Databases.Show(ctx, &ShowDatabasesOptions{Like: like})
-		if err != nil {
-			return fmt.Errorf("sweeping databases ended with error, err = %w", err)
-		}
-		var errs []error
-		log.Printf("[DEBUG] Found %d databases matching search criteria", len(dbs))
-		for idx, db := range dbs {
-			if db.Owner != "ACCOUNTADMIN" {
-				log.Printf("[DEBUG] Granting ownership on database %s, to ACCOUNTADMIN", db.ID().FullyQualifiedName())
-				err := client.Grants.GrantOwnership(
-					ctx,
-					OwnershipGrantOn{Object: &Object{
-						ObjectType: ObjectTypeDatabase,
-						Name:       db.ID(),
-					}},
-					OwnershipGrantTo{
-						AccountRoleName: Pointer(NewAccountObjectIdentifier("ACCOUNTADMIN")),
-					},
-					nil,
-				)
-				if err != nil {
-					errs = append(errs, fmt.Errorf("granting ownership on database %s ended with error, err = %w", db.ID().FullyQualifiedName(), err))
-					continue
-				}
-			}
-
-			log.Printf("[DEBUG] Processing database [%d/%d]: %s...", idx+1, len(dbs), db.ID().FullyQualifiedName())
-			if !slices.Contains(protectedDatabases, db.Name) && db.CreatedOn.Before(time.Now().Add(-8*time.Hour)) {
-				log.Printf("[DEBUG] Dropping database %s, created at: %s", db.ID().FullyQualifiedName(), db.CreatedOn.String())
-				if err := client.Databases.Drop(ctx, db.ID(), &DropDatabaseOptions{IfExists: Bool(true)}); err != nil {
-					log.Printf("[DEBUG] Dropping database %s, resulted in error %v", db.ID().FullyQualifiedName(), err)
-					errs = append(errs, fmt.Errorf("sweeping database %s ended with error, err = %w", db.ID().FullyQualifiedName(), err))
-				}
-			} else {
-				log.Printf("[DEBUG] Skipping database %s, created at: %s", db.ID().FullyQualifiedName(), db.CreatedOn.String())
-			}
-		}
-		return errors.Join(errs...)
-	}
-}
-
-func nukeUsers(client *Client) func() error {
-	protectedUsers := []string{
-		"SNOWFLAKE",
-		"ARTUR_SAWICKI",
-		"ARTUR_SAWICKI_LEGACY",
-		"JAKUB_MICHALAK",
-		"JAKUB_MICHALAK_LEGACY",
-		"JAN_CIESLAK",
-		"JAN_CIESLAK_LEGACY",
-		"TERRAFORM_SVC_ACCOUNT",
-		"TEST_CI_SERVICE_USER",
-		"PENTESTING_USER_1",
-		"PENTESTING_USER_2",
-	}
-
-	return func() error {
-		log.Println("[DEBUG] Nuking users")
-		ctx := context.Background()
-
-		users, err := client.Users.Show(ctx, &ShowUserOptions{})
-		if err != nil {
-			return fmt.Errorf("sweeping users ended with error, err = %w", err)
-		}
-		var errs []error
-		log.Printf("[DEBUG] Found %d users", len(users))
-		for idx, user := range users {
-			log.Printf("[DEBUG] Processing user [%d/%d]: %s...", idx+1, len(users), user.ID().FullyQualifiedName())
-			if !slices.Contains(protectedUsers, user.Name) && user.CreatedOn.Before(time.Now().Add(-15*time.Minute)) {
-				log.Printf("[DEBUG] Dropping user %s", user.ID().FullyQualifiedName())
-				if err := client.Users.Drop(ctx, user.ID(), &DropUserOptions{IfExists: Bool(true)}); err != nil {
-					log.Printf("[DEBUG] Dropping user %s, resulted in error %v", user.ID().FullyQualifiedName(), err)
-					errs = append(errs, fmt.Errorf("sweeping user %s ended with error, err = %w", user.ID().FullyQualifiedName(), err))
-				}
-			} else {
-				log.Printf("[DEBUG] Skipping user %s", user.ID().FullyQualifiedName())
-			}
-		}
-		return errors.Join(errs...)
-	}
 }
