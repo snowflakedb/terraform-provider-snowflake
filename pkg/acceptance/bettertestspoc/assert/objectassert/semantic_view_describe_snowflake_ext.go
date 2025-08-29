a7a50ba026acbc676f598f9a087e9f7fd55f30b3
shreyas <sarur@snowflake.com>
2025-08-29 10:29:22 -0700

    feat: add integration tests for semantic views

--- pkg/acceptance/bettertestspoc/assert/objectassert/semantic_view_describe_snowflake_ext.go
+++ pkg/acceptance/bettertestspoc/assert/objectassert/semantic_view_describe_snowflake_ext.go
@@ -8,12 +8,3 @@ import (
 type SemanticViewDetailsAssert struct {
 	*assert.SnowflakeObjectAssert[[]sdk.SemanticViewDetails, sdk.SchemaObjectIdentifier]
 }
-
-//func SemanticViewDetails(t *testing.T, id sdk.SchemaObjectIdentifier) *SemanticViewDetailsAssert {
-//	t.Helper()
-//	return &SemanticViewDetailsAssert{
-//		assert.NewSnowflakeObjectAssertWithTestClientObjectProvider(sdk.ObjectTypeSemanticView, id, func(testClient *helpers.TestClient) assert.ObjectProvider[[]sdk.SemanticViewDetails, sdk.SchemaObjectIdentifier] {
-//			return testClient.SemanticView.Describe
-//		}),
-//	}
-//}
