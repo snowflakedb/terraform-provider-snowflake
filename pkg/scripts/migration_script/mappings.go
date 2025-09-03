ea086c6c9dff8ab9a56c056e383cbccbaa933a56
Jan Cieślak <jan.cieslak@snowflake.com>
2025-09-03 13:30:35 +0200

    chore: Add more mappings for the grant migration script part1 (#3995)
    ## Changes
    - Add support for:
      - `grant_privileges_to_database_role`
      - `grant_account_role`
      - `grant_database_role`
    - Add missing Role column in the sdk.Grant (only touched the script
    part, but I'm guessing the Role mapping should be available in the SDK
    too)
    
    ## Next steps
    - Create a ticket for Role mapping in the SDK (SHOW GRANTS refactor
    issue)
    - Add other missing mappings

--- pkg/scripts/migration_script/mappings.go
+++ pkg/scripts/migration_script/mappings.go
@@ -7,22 +7,11 @@ import (
 	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/acceptance/bettertestspoc/config"
 )
 
-var resourceIdAllowedCharacters = regexp.MustCompile("[a-zA-Z0-9\\-_]+")
+var resourceIdAllowedCharacters = regexp.MustCompile("[^a-zA-Z0-9\\-_]")
 
 func NormalizeResourceId(resourceId string) string {
-	mappedResourceId := strings.Map(func(r rune) rune {
-		switch r {
-		case '.':
-			return '_'
-		default:
-			if resourceIdAllowedCharacters.MatchString(string(r)) {
-				return r
-			}
-			return -1
-		}
-	}, resourceId)
-
-	return "snowflake_generated_" + mappedResourceId
+	resourceId = strings.ReplaceAll(resourceId, `.`, "_")
+	return "snowflake_generated_" + string(resourceIdAllowedCharacters.ReplaceAll([]byte(resourceId), []byte("")))
 }
 
 // ResourceFromModel is a copy of config.ResourceFromModel function, but it doesn't use testing.T internally.
