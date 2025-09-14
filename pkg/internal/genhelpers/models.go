8c3e94137f17d7c69540f3f5e225f18335598d23
Artur Sawicki <artur.sawicki@snowflake.com>
2025-09-14 23:45:03 +0200

    Add preamble model constructor

--- pkg/internal/genhelpers/models.go
+++ pkg/internal/genhelpers/models.go
@@ -1,8 +1,20 @@
 package genhelpers
 
+import "os"
+
 type PreambleModel struct {
 	GeneratorName    string
 	GeneratorVersion string
 	PackageName      string
 	ExplicitImports  []string
+}
+
+func NewPreambleModel(name string, version string, imports []string) *PreambleModel {
+	packageWithGenerateDirective := os.Getenv("GOPACKAGE")
+	return &PreambleModel{
+		GeneratorName:    name,
+		GeneratorVersion: version,
+		PackageName:      packageWithGenerateDirective,
+		ExplicitImports:  imports,
+	}
 }
