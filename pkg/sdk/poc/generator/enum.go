4703f78627e34813aa38a8281dae77c34504e0ec
Jakub Michalak <jakub.michalak@snowflake.com>
2025-09-19 14:57:58 +0200

    Changes in enums

--- pkg/sdk/poc/generator/enum.go
+++ pkg/sdk/poc/generator/enum.go
@@ -1,47 +1,39 @@
 package generator
 
-import (
-	"strings"
-)
+import "github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/internal/stringhelpers"
 
 // Enum defines an enum type with its name and values. For now, only string values are supported.
+// Limitations (also added to the README.md):
+// Generate unit tests for the enum type converters
+// Handle synonyms (e.g. [`ToWarehouseSize`](https://github.com/snowflakedb/terraform-provider-snowflake/blob/5bdcd127d9288212b10ea7b138bebc0cb770c5b9/pkg/sdk/warehouses.go#L77))
 type Enum struct {
 	Name       string
 	NamePlural string
 	Values     []string
 }
 
-func NewEnum(name string, values ...string) *Enum {
+func NewEnum(name, namePlural string, values ...string) *Enum {
 	return &Enum{
-		Name:   name,
-		Values: values,
+		Name:       name,
+		NamePlural: namePlural,
+		Values:     values,
 	}
 }
 
 // valueName returns the constant name for a given enum value.
 // E.g. for type ProgrammaticAccessTokenStatus and value "ACTIVE_VALUE" -> "ProgrammaticAccessTokenStatusActiveValue".
 func (e *Enum) valueName(value string) string {
-	normalized := strings.ToLower(value)
-	parts := strings.Split(normalized, "_")
-	for i, part := range parts {
-		if len(part) > 0 {
-			parts[i] = strings.ToUpper(part[:1]) + part[1:]
-		}
-	}
-	return e.Name + strings.Join(parts, "")
+	return e.Name + stringhelpers.SnakeCaseToCamel(value)
 }
 
 // AllValuesSliceName returns the name of the slice containing all enum values
 func (e *Enum) AllValuesSliceName() string {
-	if e.NamePlural == "" {
-		return "all" + e.Name + "s"
-	}
-	return "all" + e.NamePlural
+	return "All" + e.NamePlural
 }
 
 // ConverterFunctionName returns the name of the converter function
 func (e *Enum) ConverterFunctionName() string {
-	return "to" + e.Name
+	return "To" + e.Name
 }
 
 // ValueRepresentations returns all enum values with their Go names and values used in the template.
