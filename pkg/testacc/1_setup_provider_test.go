88473089be8e9128a3a43a7f2dc0a31b365bf174
shreyas <sarur@snowflake.com>
2025-10-21 10:06:39 -0700

    remove from test provider

--- pkg/testacc/1_setup_provider_test.go
+++ pkg/testacc/1_setup_provider_test.go
@@ -5,12 +5,10 @@ import (
 	"fmt"
 
 	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/acceptance/testenvs"
-	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/datasources"
 	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/internal/oswrapper"
 	internalprovider "github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/internal/provider"
 	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/provider"
 	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/provider/previewfeatures"
-	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/resources"
 	"github.com/hashicorp/terraform-plugin-go/tfprotov5"
 	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
 	"github.com/hashicorp/terraform-plugin-mux/tf5to6server"
@@ -46,8 +44,6 @@ func setUpProvider() error {
 	providerInitializationCache = make(map[string]cacheEntry)
 
 	TestAccProvider = provider.Provider()
-	TestAccProvider.ResourcesMap["snowflake_semantic_view"] = resources.SemanticView()
-	TestAccProvider.DataSourcesMap["snowflake_semantic_views"] = datasources.SemanticViews()
 	TestAccProvider.ConfigureContextFunc = configureProviderWithConfigCache
 
 	var err error
