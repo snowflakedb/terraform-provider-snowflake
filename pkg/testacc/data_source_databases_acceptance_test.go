637acf1b3b51d918a762bf803e4375a46c6be695
Jan Cieślak <jan.cieslak@snowflake.com>
2025-10-21 19:17:56 +0200

    Adjust databases data sources acceptance tests to set the common testing pattern

--- pkg/testacc/data_source_databases_acceptance_test.go
+++ pkg/testacc/data_source_databases_acceptance_test.go
@@ -8,6 +8,8 @@ import (
 
 	accconfig "github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/acceptance/bettertestspoc/config"
 
+	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/acceptance/bettertestspoc/assert"
+	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/acceptance/bettertestspoc/assert/resourceshowoutputassert"
 	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/acceptance/bettertestspoc/config/datasourcemodel"
 	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/acceptance/bettertestspoc/config/model"
 	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/acceptance/helpers/random"
@@ -16,103 +18,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-testing/tfversion"
 )
 
-func TestAcc_Databases_Complete(t *testing.T) {
-	databaseId := testClient().Ids.RandomAccountObjectIdentifier()
-	databaseName := databaseId.Name()
-	comment := random.Comment()
-	secondaryAccountId := secondaryTestClient().Account.GetAccountIdentifier(t)
-
-	databaseModel := model.DatabaseWithParametersSet("test", databaseName).
-		WithComment(comment).
-		WithReplication(secondaryAccountId, true, true)
-	databasesModel := datasourcemodel.Databases("test").
-		WithLike(databaseName).
-		WithStartsWith(databaseName).
-		WithLimit(1).
-		WithDependsOn(databaseModel.ResourceReference())
-	databasesWithoutOptionalsModel := datasourcemodel.Databases("test").
-		WithLike(databaseName).
-		WithStartsWith(databaseName).
-		WithLimit(1).
-		WithDependsOn(databaseModel.ResourceReference()).
-		WithWithDescribe(false).
-		WithWithParameters(false)
-
-	resource.Test(t, resource.TestCase{
-		ProtoV6ProviderFactories: TestAccProtoV6ProviderFactories,
-		TerraformVersionChecks: []tfversion.TerraformVersionCheck{
-			tfversion.RequireAbove(tfversion.Version1_5_0),
-		},
-		CheckDestroy: CheckDestroy(t, resources.Database),
-		Steps: []resource.TestStep{
-			{
-				Config: accconfig.FromModels(t, databaseModel, databasesModel),
-				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr(databasesModel.DatasourceReference(), "databases.#", "1"),
-					resource.TestCheckResourceAttrSet(databasesModel.DatasourceReference(), "databases.0.show_output.0.created_on"),
-					resource.TestCheckResourceAttr(databasesModel.DatasourceReference(), "databases.0.show_output.0.name", databaseName),
-					resource.TestCheckResourceAttr(databasesModel.DatasourceReference(), "databases.0.show_output.0.kind", "STANDARD"),
-					resource.TestCheckResourceAttr(databasesModel.DatasourceReference(), "databases.0.show_output.0.transient", "false"),
-					resource.TestCheckResourceAttr(databasesModel.DatasourceReference(), "databases.0.show_output.0.is_default", "false"),
-					resource.TestCheckResourceAttr(databasesModel.DatasourceReference(), "databases.0.show_output.0.is_current", "true"),
-					resource.TestCheckResourceAttr(databasesModel.DatasourceReference(), "databases.0.show_output.0.origin", ""),
-					resource.TestCheckResourceAttrSet(databasesModel.DatasourceReference(), "databases.0.show_output.0.owner"),
-					resource.TestCheckResourceAttr(databasesModel.DatasourceReference(), "databases.0.show_output.0.comment", comment),
-					resource.TestCheckResourceAttr(databasesModel.DatasourceReference(), "databases.0.show_output.0.options", ""),
-					resource.TestCheckResourceAttrSet(databasesModel.DatasourceReference(), "databases.0.show_output.0.retention_time"),
-					resource.TestCheckResourceAttr(databasesModel.DatasourceReference(), "databases.0.show_output.0.resource_group", ""),
-					resource.TestCheckResourceAttrSet(databasesModel.DatasourceReference(), "databases.0.show_output.0.owner_role_type"),
-
-					resource.TestCheckResourceAttr(databasesModel.DatasourceReference(), "databases.0.describe_output.#", "2"),
-					resource.TestCheckResourceAttrSet(databasesModel.DatasourceReference(), "databases.0.describe_output.0.created_on"),
-					resource.TestCheckResourceAttrSet(databasesModel.DatasourceReference(), "databases.0.describe_output.0.name"),
-					resource.TestCheckResourceAttr(databasesModel.DatasourceReference(), "databases.0.describe_output.0.kind", "SCHEMA"),
-
-					resource.TestCheckResourceAttr(databasesModel.DatasourceReference(), "databases.0.parameters.#", "1"),
-					resource.TestCheckResourceAttrSet(databasesModel.DatasourceReference(), "databases.0.parameters.0.data_retention_time_in_days.0.value"),
-					resource.TestCheckResourceAttrSet(databasesModel.DatasourceReference(), "databases.0.parameters.0.max_data_extension_time_in_days.0.value"),
-					resource.TestCheckResourceAttr(databasesModel.DatasourceReference(), "databases.0.parameters.0.external_volume.0.value", ""),
-					resource.TestCheckResourceAttrSet(databasesModel.DatasourceReference(), "databases.0.parameters.0.catalog.0.value"),
-					resource.TestCheckResourceAttrSet(databasesModel.DatasourceReference(), "databases.0.parameters.0.replace_invalid_characters.0.value"),
-					resource.TestCheckResourceAttr(databasesModel.DatasourceReference(), "databases.0.parameters.0.default_ddl_collation.0.value", ""),
-					resource.TestCheckResourceAttrSet(databasesModel.DatasourceReference(), "databases.0.parameters.0.storage_serialization_policy.0.value"),
-					resource.TestCheckResourceAttrSet(databasesModel.DatasourceReference(), "databases.0.parameters.0.log_level.0.value"),
-					resource.TestCheckResourceAttrSet(databasesModel.DatasourceReference(), "databases.0.parameters.0.trace_level.0.value"),
-					resource.TestCheckResourceAttrSet(databasesModel.DatasourceReference(), "databases.0.parameters.0.suspend_task_after_num_failures.0.value"),
-					resource.TestCheckResourceAttrSet(databasesModel.DatasourceReference(), "databases.0.parameters.0.task_auto_retry_attempts.0.value"),
-					resource.TestCheckResourceAttrSet(databasesModel.DatasourceReference(), "databases.0.parameters.0.user_task_managed_initial_warehouse_size.0.value"),
-					resource.TestCheckResourceAttrSet(databasesModel.DatasourceReference(), "databases.0.parameters.0.user_task_minimum_trigger_interval_in_seconds.0.value"),
-					resource.TestCheckResourceAttrSet(databasesModel.DatasourceReference(), "databases.0.parameters.0.quoted_identifiers_ignore_case.0.value"),
-					resource.TestCheckResourceAttrSet(databasesModel.DatasourceReference(), "databases.0.parameters.0.enable_console_output.0.value"),
-				),
-			},
-			{
-				Config: accconfig.FromModels(t, databaseModel, databasesWithoutOptionalsModel),
-				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr(databasesModel.DatasourceReference(), "databases.#", "1"),
-					resource.TestCheckResourceAttrSet(databasesModel.DatasourceReference(), "databases.0.show_output.0.created_on"),
-					resource.TestCheckResourceAttr(databasesModel.DatasourceReference(), "databases.0.show_output.0.name", databaseName),
-					resource.TestCheckResourceAttr(databasesModel.DatasourceReference(), "databases.0.show_output.0.kind", "STANDARD"),
-					resource.TestCheckResourceAttr(databasesModel.DatasourceReference(), "databases.0.show_output.0.transient", "false"),
-					resource.TestCheckResourceAttr(databasesModel.DatasourceReference(), "databases.0.show_output.0.is_default", "false"),
-					resource.TestCheckResourceAttr(databasesModel.DatasourceReference(), "databases.0.show_output.0.is_current", "true"),
-					resource.TestCheckResourceAttr(databasesModel.DatasourceReference(), "databases.0.show_output.0.origin", ""),
-					resource.TestCheckResourceAttrSet(databasesModel.DatasourceReference(), "databases.0.show_output.0.owner"),
-					resource.TestCheckResourceAttr(databasesModel.DatasourceReference(), "databases.0.show_output.0.comment", comment),
-					resource.TestCheckResourceAttr(databasesModel.DatasourceReference(), "databases.0.show_output.0.options", ""),
-					resource.TestCheckResourceAttrSet(databasesModel.DatasourceReference(), "databases.0.show_output.0.retention_time"),
-					resource.TestCheckResourceAttr(databasesModel.DatasourceReference(), "databases.0.show_output.0.resource_group", ""),
-					resource.TestCheckResourceAttrSet(databasesModel.DatasourceReference(), "databases.0.show_output.0.owner_role_type"),
-
-					resource.TestCheckResourceAttr(databasesModel.DatasourceReference(), "databases.0.describe_output.#", "0"),
-					resource.TestCheckResourceAttr(databasesModel.DatasourceReference(), "databases.0.parameters.#", "0"),
-				),
-			},
-		},
-	})
-}
-
-func TestAcc_Databases_DifferentFiltering(t *testing.T) {
+func TestAcc_Databases_BasicUseCase_DifferentFiltering(t *testing.T) {
 	prefix := random.AlphaN(4)
 	idOne := testClient().Ids.RandomAccountObjectIdentifierWithPrefix(prefix)
 	idTwo := testClient().Ids.RandomAccountObjectIdentifierWithPrefix(prefix)
@@ -154,6 +60,102 @@ func TestAcc_Databases_DifferentFiltering(t *testing.T) {
 				Config: accconfig.FromModels(t, databaseModel1, databaseModel2, databaseModel3, databasesWithLimitModel),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr(databasesWithLikeModel.DatasourceReference(), "databases.#", "1"),
+				),
+			},
+		},
+	})
+}
+
+func TestAcc_Databases_CompleteUseCase(t *testing.T) {
+	databaseId := testClient().Ids.RandomAccountObjectIdentifier()
+	databaseName := databaseId.Name()
+	comment := random.Comment()
+	secondaryAccountId := secondaryTestClient().Account.GetAccountIdentifier(t)
+
+	databaseModel := model.DatabaseWithParametersSet("test", databaseName).
+		WithComment(comment).
+		WithReplication(secondaryAccountId, true, true)
+	databasesModel := datasourcemodel.Databases("test").
+		WithLike(databaseName).
+		WithStartsWith(databaseName).
+		WithLimit(1).
+		WithDependsOn(databaseModel.ResourceReference())
+	databasesWithoutOptionalsModel := datasourcemodel.Databases("test").
+		WithLike(databaseName).
+		WithStartsWith(databaseName).
+		WithLimit(1).
+		WithDependsOn(databaseModel.ResourceReference()).
+		WithWithDescribe(false).
+		WithWithParameters(false)
+
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: TestAccProtoV6ProviderFactories,
+		TerraformVersionChecks: []tfversion.TerraformVersionCheck{
+			tfversion.RequireAbove(tfversion.Version1_5_0),
+		},
+		CheckDestroy: CheckDestroy(t, resources.Database),
+		Steps: []resource.TestStep{
+			{
+				Config: accconfig.FromModels(t, databaseModel, databasesModel),
+				Check: assertThat(t,
+					resourceshowoutputassert.DatabasesDatasourceShowOutput(t, databasesModel.DatasourceReference()).
+						HasCreatedOnNotEmpty().
+						HasName(databaseName).
+						HasKind("STANDARD").
+						HasTransient(false).
+						HasIsDefault(false).
+						HasIsCurrent(true).
+						HasOriginEmpty().
+						HasOwnerNotEmpty().
+						HasComment(comment).
+						HasOptions("").
+						HasRetentionTimeNotEmpty().
+						HasResourceGroup("").
+						HasOwnerRoleTypeNotEmpty(),
+
+					assert.Check(resource.TestCheckResourceAttr(databasesModel.DatasourceReference(), "databases.0.describe_output.#", "2")),
+					assert.Check(resource.TestCheckResourceAttrSet(databasesModel.DatasourceReference(), "databases.0.describe_output.0.created_on")),
+					assert.Check(resource.TestCheckResourceAttrSet(databasesModel.DatasourceReference(), "databases.0.describe_output.0.name")),
+					assert.Check(resource.TestCheckResourceAttr(databasesModel.DatasourceReference(), "databases.0.describe_output.0.kind", "SCHEMA")),
+
+					assert.Check(resource.TestCheckResourceAttr(databasesModel.DatasourceReference(), "databases.0.parameters.#", "1")),
+					assert.Check(resource.TestCheckResourceAttrSet(databasesModel.DatasourceReference(), "databases.0.parameters.0.data_retention_time_in_days.0.value")),
+					assert.Check(resource.TestCheckResourceAttrSet(databasesModel.DatasourceReference(), "databases.0.parameters.0.max_data_extension_time_in_days.0.value")),
+					assert.Check(resource.TestCheckResourceAttr(databasesModel.DatasourceReference(), "databases.0.parameters.0.external_volume.0.value", "")),
+					assert.Check(resource.TestCheckResourceAttrSet(databasesModel.DatasourceReference(), "databases.0.parameters.0.catalog.0.value")),
+					assert.Check(resource.TestCheckResourceAttrSet(databasesModel.DatasourceReference(), "databases.0.parameters.0.replace_invalid_characters.0.value")),
+					assert.Check(resource.TestCheckResourceAttr(databasesModel.DatasourceReference(), "databases.0.parameters.0.default_ddl_collation.0.value", "")),
+					assert.Check(resource.TestCheckResourceAttrSet(databasesModel.DatasourceReference(), "databases.0.parameters.0.storage_serialization_policy.0.value")),
+					assert.Check(resource.TestCheckResourceAttrSet(databasesModel.DatasourceReference(), "databases.0.parameters.0.log_level.0.value")),
+					assert.Check(resource.TestCheckResourceAttrSet(databasesModel.DatasourceReference(), "databases.0.parameters.0.trace_level.0.value")),
+					assert.Check(resource.TestCheckResourceAttrSet(databasesModel.DatasourceReference(), "databases.0.parameters.0.suspend_task_after_num_failures.0.value")),
+					assert.Check(resource.TestCheckResourceAttrSet(databasesModel.DatasourceReference(), "databases.0.parameters.0.task_auto_retry_attempts.0.value")),
+					assert.Check(resource.TestCheckResourceAttrSet(databasesModel.DatasourceReference(), "databases.0.parameters.0.user_task_managed_initial_warehouse_size.0.value")),
+					assert.Check(resource.TestCheckResourceAttrSet(databasesModel.DatasourceReference(), "databases.0.parameters.0.user_task_minimum_trigger_interval_in_seconds.0.value")),
+					assert.Check(resource.TestCheckResourceAttrSet(databasesModel.DatasourceReference(), "databases.0.parameters.0.quoted_identifiers_ignore_case.0.value")),
+					assert.Check(resource.TestCheckResourceAttrSet(databasesModel.DatasourceReference(), "databases.0.parameters.0.enable_console_output.0.value")),
+				),
+			},
+			{
+				Config: accconfig.FromModels(t, databaseModel, databasesWithoutOptionalsModel),
+				Check: assertThat(t,
+					resourceshowoutputassert.DatabasesDatasourceShowOutput(t, databasesModel.DatasourceReference()).
+						HasCreatedOnNotEmpty().
+						HasName(databaseName).
+						HasKind("STANDARD").
+						HasTransient(false).
+						HasIsDefault(false).
+						HasIsCurrent(true).
+						HasOriginEmpty().
+						HasOwnerNotEmpty().
+						HasComment(comment).
+						HasOptions("").
+						HasRetentionTimeNotEmpty().
+						HasResourceGroup("").
+						HasOwnerRoleTypeNotEmpty(),
+
+					assert.Check(resource.TestCheckResourceAttr(databasesModel.DatasourceReference(), "databases.0.describe_output.#", "0")),
+					assert.Check(resource.TestCheckResourceAttr(databasesModel.DatasourceReference(), "databases.0.parameters.#", "0")),
 				),
 			},
 		},
