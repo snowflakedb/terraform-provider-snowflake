18ca467ebaa24db7243e66f8044097f2c1b5c19f
Artur Sawicki <artur.sawicki@snowflake.com>
2025-10-22 11:10:10 +0200

    Remove old generate directive from sequences

--- pkg/sdk/poc/main.go
+++ pkg/sdk/poc/main.go
@@ -35,7 +35,6 @@ var definitionMapping = map[string]*generator.Interface{
 	"managed_accounts_def.go":                sdk.ManagedAccountsDef,
 	"row_access_policies_def.go":             sdk.RowAccessPoliciesDef,
 	"applications_def.go":                    sdk.ApplicationsDef,
-	"sequences_def.go":                       sdk.SequencesDef,
 	"materialized_views_def.go":              sdk.MaterializedViewsDef,
 	"api_integrations_def.go":                sdk.ApiIntegrationsDef,
 	"notification_integrations_def.go":       sdk.NotificationIntegrationsDef,
