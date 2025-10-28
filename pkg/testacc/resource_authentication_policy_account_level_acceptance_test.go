50e4cdd463dcd3d2681fe6410c5ac985fe8a859c
Jakub Michalak <jakub.michalak@snowflake.com>
2025-10-28 12:40:53 +0100

    chore: Rework authentication policy resource (#4125)
    - Adjust the resource to v1 guidelines.
    - Fix object renaming.
    - Remove deprecated `mfa_authentication_methods` field from examples.
    - Generate builders & assertions. Use them in acc tests.
    - Add more acc tests.
    - Add SDK helpers.
    
    ## TODO
    - Add missing blocks for pat/wif/mfa policies.
    - Add data source.

---
+++ pkg/testacc/resource_authentication_policy_account_level_acceptance_test.go
@@ -0,0 +1,91 @@
+//go:build account_level_tests
+
+package testacc
+
+import (
+	"testing"
+
+	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/acceptance/bettertestspoc/assert"
+	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/acceptance/bettertestspoc/assert/resourceassert"
+	accconfig "github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/acceptance/bettertestspoc/config"
+	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/acceptance/bettertestspoc/config/model"
+	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/acceptance/bettertestspoc/config/providermodel"
+	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/acceptance/testenvs"
+	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/acceptance/testprofiles"
+	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/provider/resources"
+	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/sdk"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/plancheck"
+	"github.com/hashicorp/terraform-plugin-testing/tfversion"
+)
+
+func TestAcc_AuthenticationPolicies_AccountLevel(t *testing.T) {
+	t.Setenv(string(testenvs.ConfigureClientOnce), "")
+
+	client := secondaryTestClient()
+	client.BcrBundles.DisableBcrBundle(t, "2025_06")
+
+	id := client.Ids.RandomSchemaObjectIdentifier()
+	basicModel := model.AuthenticationPolicy("test", id.DatabaseName(), id.SchemaName(), id.Name())
+	completeModel := model.AuthenticationPolicy("test", id.DatabaseName(), id.SchemaName(), id.Name()).
+		WithMfaAuthenticationMethods(sdk.MfaAuthenticationMethodsPassword, sdk.MfaAuthenticationMethodsSaml)
+	providerModel := providermodel.SnowflakeProvider().
+		WithProfile(testprofiles.Secondary)
+	resource.Test(t, resource.TestCase{
+		TerraformVersionChecks: []tfversion.TerraformVersionCheck{
+			tfversion.RequireAbove(tfversion.Version1_5_0),
+		},
+		CheckDestroy:             CheckDestroy(t, resources.AuthenticationPolicy),
+		ProtoV6ProviderFactories: TestAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: accconfig.FromModels(t, providerModel, basicModel),
+				Check: assertThat(t,
+					resourceassert.AuthenticationPolicyResource(t, basicModel.ResourceReference()).
+						HasMfaAuthenticationMethodsEmpty(),
+					assert.Check(resource.TestCheckResourceAttr(basicModel.ResourceReference(), "describe_output.#", "1")),
+					assert.Check(resource.TestCheckResourceAttr(basicModel.ResourceReference(), "describe_output.0.mfa_authentication_methods", "[PASSWORD]")),
+				),
+			},
+			{
+				Config: accconfig.FromModels(t, providerModel, completeModel),
+				Check: assertThat(t,
+					resourceassert.AuthenticationPolicyResource(t, completeModel.ResourceReference()).
+						HasMfaAuthenticationMethods(sdk.MfaAuthenticationMethodsPassword, sdk.MfaAuthenticationMethodsSaml),
+					assert.Check(resource.TestCheckResourceAttr(completeModel.ResourceReference(), "describe_output.#", "1")),
+					assert.Check(resource.TestCheckResourceAttr(completeModel.ResourceReference(), "describe_output.0.mfa_authentication_methods", "[SAML, PASSWORD]")),
+				),
+			},
+			{
+				Config: accconfig.FromModels(t, providerModel, completeModel),
+				Taint:  []string{completeModel.ResourceReference()},
+				Check: assertThat(t,
+					resourceassert.AuthenticationPolicyResource(t, completeModel.ResourceReference()).
+						HasMfaAuthenticationMethods(sdk.MfaAuthenticationMethodsPassword, sdk.MfaAuthenticationMethodsSaml),
+					assert.Check(resource.TestCheckResourceAttr(completeModel.ResourceReference(), "describe_output.#", "1")),
+					assert.Check(resource.TestCheckResourceAttr(completeModel.ResourceReference(), "describe_output.0.mfa_authentication_methods", "[SAML, PASSWORD]")),
+				),
+			},
+			{
+				PreConfig: func() {
+					client.AuthenticationPolicy.Alter(t, sdk.NewAlterAuthenticationPolicyRequest(id).WithSet(
+						*sdk.NewAuthenticationPolicySetRequest().
+							WithMfaAuthenticationMethods([]sdk.MfaAuthenticationMethods{{Method: sdk.MfaAuthenticationMethodsSaml}}),
+					))
+				},
+				Config: accconfig.FromModels(t, providerModel, completeModel),
+				ConfigPlanChecks: resource.ConfigPlanChecks{
+					PreApply: []plancheck.PlanCheck{
+						plancheck.ExpectResourceAction(completeModel.ResourceReference(), plancheck.ResourceActionUpdate),
+					},
+				},
+				Check: assertThat(t,
+					resourceassert.AuthenticationPolicyResource(t, completeModel.ResourceReference()).
+						HasMfaAuthenticationMethods(sdk.MfaAuthenticationMethodsPassword, sdk.MfaAuthenticationMethodsSaml),
+					assert.Check(resource.TestCheckResourceAttr(completeModel.ResourceReference(), "describe_output.#", "1")),
+					assert.Check(resource.TestCheckResourceAttr(completeModel.ResourceReference(), "describe_output.0.mfa_authentication_methods", "[SAML, PASSWORD]")),
+				),
+			},
+		},
+	})
+}
