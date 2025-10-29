e690b1630d79818e7cf50d265ea60a760de6cdc9
Artur Sawicki <artur.sawicki@snowflake.com>
2025-10-29 09:54:23 +0100

    Add TODO

--- pkg/provider/experimentalfeatures/experimental_features.go
+++ pkg/provider/experimentalfeatures/experimental_features.go
@@ -21,6 +21,7 @@ var AllExperimentalFeatures = sdk.AsStringList(allExperimentalFeatures)
 
 // TODO [this PR]: test
 // TODO [this PR]: Describe logic for disabliong experiments on the provider side and adjusting the implementation
+// TODO [next PR]: Add documentation for such experimental features in the provider's docs automatically
 func IsExperimentEnabled(experiment feature, enabledExperiments []string) bool {
 	return !slices.ContainsFunc(enabledExperiments, func(s string) bool {
 		return strings.EqualFold(string(experiment), s)
