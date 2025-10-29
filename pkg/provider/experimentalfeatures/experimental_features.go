436fc51994a2733aeea66d32b794b765b6238d93
Artur Sawicki <artur.sawicki@snowflake.com>
2025-10-29 09:53:32 +0100

    Add conditional warehouse query logic

--- pkg/provider/experimentalfeatures/experimental_features.go
+++ pkg/provider/experimentalfeatures/experimental_features.go
@@ -1,17 +1,28 @@
 package experimentalfeatures
 
 import (
+	"slices"
+	"strings"
+
 	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/sdk"
 )
 
 type feature string
 
 const (
-	ImprovedWarehouseShowQuery feature = "ImprovedWarehouseShowQuery"
+	WarehouseShowImprovedPerformance feature = "WAREHOUSE_SHOW_IMPROVED_PERFORMANCE"
 )
 
 var allExperimentalFeatures = []feature{
-	ImprovedWarehouseShowQuery,
+	WarehouseShowImprovedPerformance,
 }
 
 var AllExperimentalFeatures = sdk.AsStringList(allExperimentalFeatures)
+
+// TODO [this PR]: test
+// TODO [this PR]: Describe logic for disabliong experiments on the provider side and adjusting the implementation
+func IsExperimentEnabled(experiment feature, enabledExperiments []string) bool {
+	return !slices.ContainsFunc(enabledExperiments, func(s string) bool {
+		return strings.EqualFold(string(experiment), s)
+	})
+}
