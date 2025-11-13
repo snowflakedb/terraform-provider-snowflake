5f0f93a009bb8e43d2fbe2fb092676b63ab1e90a
Artur Sawicki <artur.sawicki@snowflake.com>
2025-11-13 10:42:48 +0100

    chore: Regenerate SDK without package problems (#4183)
    The main problems solved by this change:
    - Compilation problems during the regeneration/clean regeneration -
    non-existent interfaces, types - e.g. from ext (SDK generator is no
    longer depending on the SDK package which was the main source of issue)
    - No separation between example and production SDK objects - causing
    problems for both generations (the lists are now separate and only one
    is picked based on the build tag from the `generate.go` file)
    
    Changes:
    - Remove old generator logic and use the reworked one (only Sequences
    moved currently)
    - Extract two `0_init.go` files to keep SDK and example definitions
    separately. `generate.go` files are adjusted to run with a proper tag.
    - Merge `example` and `rework` packages
    - Merge sdk_definitions from `example` and `rework` packages
    - Clean up Makefile and README
    - Remove `rework` Makefile and README
    - Regenerate all examples and Sequences
    - Clean up SDK generation packages to be more consistent with other
    generators, new layout:
      - `sdk`
        - `generator`
          - `defs`
          - `example`
          - `gen`
            - `main`
            - `templates`
            - ... all generator specific files...
          - `README.md`
        - `generate.go`
        - ... all generated and manually added SDK files...
    
    Next PRs:
    - move the rest of the SDK definitions (definition files contain enum
    definitions - they need to stay in the SDK package; move some manually
    added stuff to ext files - as now it won't stop the regeneration)
    - clean up essentials/improvements/known issues README sections
    - bring back convenience Makefile recipes for a single object removal
    and generation (now the generation can be achieved with the object
    filtering, but because it's based on the names and not file names now,
    the file removal is not that trivial)

--- pkg/sdk/functions_def.go
+++ pkg/sdk/functions_def.go
@@ -1,6 +1,6 @@
 package sdk
 
-import g "github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/sdk/poc/generator"
+import g "github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/sdk/generator/gen"
 
 var functionArgument = g.NewQueryStruct("FunctionArgument").
 	Text("ArgName", g.KeywordOptions().DoubleQuotes().Required()).
---
+++ pkg/sdk/generator/gen/show_object_methods.go
@@ -0,0 +1,61 @@
+package gen
+
+import (
+	"log"
+	"slices"
+)
+
+type ShowObjectIdMethod struct {
+	StructName     string
+	IdentifierKind objectIdentifierKind
+	Args           []string
+}
+
+func NewShowObjectIDMethod(structName string, idType objectIdentifierKind) *ShowObjectIdMethod {
+	return &ShowObjectIdMethod{
+		StructName:     structName,
+		IdentifierKind: idType,
+		Args:           idTypeParts[idType],
+	}
+}
+
+var idTypeParts map[objectIdentifierKind][]string = map[objectIdentifierKind][]string{
+	AccountObjectIdentifier:  {"Name"},
+	DatabaseObjectIdentifier: {"DatabaseName", "Name"},
+	SchemaObjectIdentifier:   {"DatabaseName", "SchemaName", "Name"},
+}
+
+// TODO [SNOW-2324252]: do we need to search for this struct? Maybe we can have it more easily?
+func CheckRequiredFieldsForIdMethod(structName string, helperStructs []*Field, idKind objectIdentifierKind) bool {
+	if requiredFields, ok := idTypeParts[idKind]; ok {
+		for _, field := range helperStructs {
+			if field.Name == structName {
+				return containsFieldNames(field.Fields, requiredFields...)
+			}
+		}
+	}
+	log.Printf("[WARN] no required fields mapping defined for identifier %s", idKind)
+	return false
+}
+
+func containsFieldNames(fields []*Field, names ...string) bool {
+	fieldNames := []string{}
+	for _, field := range fields {
+		fieldNames = append(fieldNames, field.Name)
+	}
+
+	for _, name := range names {
+		if !slices.Contains(fieldNames, name) {
+			return false
+		}
+	}
+	return true
+}
+
+type ShowObjectTypeMethod struct {
+	StructName string
+}
+
+func NewShowObjectTypeMethod(structName string) *ShowObjectTypeMethod {
+	return &ShowObjectTypeMethod{StructName: structName}
+}
