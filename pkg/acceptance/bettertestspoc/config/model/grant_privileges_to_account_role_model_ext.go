81db602fa7866cc2e6ce8a92b96c2c75c8cadc11
Jan Cieślak <jan.cieslak@snowflake.com>
2025-12-11 16:26:03 +0100

    wip

--- pkg/acceptance/bettertestspoc/config/model/grant_privileges_to_account_role_model_ext.go
+++ pkg/acceptance/bettertestspoc/config/model/grant_privileges_to_account_role_model_ext.go
@@ -2,11 +2,58 @@ package model
 
 import (
 	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/internal/collections"
+	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/sdk"
 	"github.com/hashicorp/terraform-plugin-testing/config"
 )
 
-func (g *GrantPrivilegesToAccountRoleModel) WithPrivileges(privileges []string) *GrantPrivilegesToAccountRoleModel {
+func (g *GrantPrivilegesToAccountRoleModel) WithPrivileges(privileges ...string) *GrantPrivilegesToAccountRoleModel {
 	privilegeStringVariables := collections.Map(privileges, func(privilege string) config.Variable { return config.StringVariable(privilege) })
 	g.WithPrivilegesValue(config.ListVariable(privilegeStringVariables...))
+	return g
+}
+
+func (g *GrantPrivilegesToAccountRoleModel) WithOnAccountObject(objectType sdk.ObjectType, id sdk.AccountObjectIdentifier) *GrantPrivilegesToAccountRoleModel {
+	g.WithOnAccountObjectValue(config.ObjectVariable(map[string]config.Variable{
+		"object_type": config.StringVariable(objectType.String()),
+		"object_name": config.StringVariable(id.Name()),
+	}))
+	return g
+}
+
+func (g *GrantPrivilegesToAccountRoleModel) WithOnAllSchemasInDatabase(id sdk.AccountObjectIdentifier) *GrantPrivilegesToAccountRoleModel {
+	g.WithOnSchemaValue(config.ObjectVariable(map[string]config.Variable{
+		"all_schemas_in_database": config.StringVariable(id.FullyQualifiedName()),
+	}))
+	return g
+}
+
+func (g *GrantPrivilegesToAccountRoleModel) WithOnFutureSchemasInDatabase(id sdk.AccountObjectIdentifier) *GrantPrivilegesToAccountRoleModel {
+	g.WithOnSchemaValue(config.ObjectVariable(map[string]config.Variable{
+		"future_schemas_in_database": config.StringVariable(id.FullyQualifiedName()),
+	}))
+	return g
+}
+
+func (g *GrantPrivilegesToAccountRoleModel) WithOnAllSchemaObjectsInSchema(pluralObjectType sdk.PluralObjectType, schemaId sdk.DatabaseObjectIdentifier) *GrantPrivilegesToAccountRoleModel {
+	g.WithOnSchemaObjectValue(config.ObjectVariable(map[string]config.Variable{
+		"all": config.ListVariable(
+			config.ObjectVariable(map[string]config.Variable{
+				"object_type_plural": config.StringVariable(string(pluralObjectType)),
+				"in_schema":          config.StringVariable(schemaId.FullyQualifiedName()),
+			}),
+		),
+	}))
+	return g
+}
+
+func (g *GrantPrivilegesToAccountRoleModel) WithOnFutureSchemaObjectsInSchema(pluralObjectType sdk.PluralObjectType, schemaId sdk.DatabaseObjectIdentifier) *GrantPrivilegesToAccountRoleModel {
+	g.WithOnSchemaObjectValue(config.ObjectVariable(map[string]config.Variable{
+		"future": config.ListVariable(
+			config.ObjectVariable(map[string]config.Variable{
+				"object_type_plural": config.StringVariable(string(pluralObjectType)),
+				"in_schema":          config.StringVariable(schemaId.FullyQualifiedName()),
+			}),
+		),
+	}))
 	return g
 }
