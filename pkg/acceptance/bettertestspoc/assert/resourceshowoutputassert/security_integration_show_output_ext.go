069dccb0ba939b48f3d600c2b983d4a37a35b173
Jan Cieślak <jan.cieslak@snowflake.com>
2025-12-11 11:58:03 +0100

    chore: Add conditional assertions (#4259)
    ## Changes
    - Introduce a new env that would indicate Snowflake environment where
    the tests are run (currently: PROD and NON_PROD)
    - Add a bit of abstraction over the env (use PROD as default)
    - Adjust Docker Compose to use the appropriate NON_PROD Snowflake
    environment value
    - Adjust failing tests due to environment differences
    - Added a new SnowflakeDefaults client in TestClient struct and used in
    tests
    
    ## Next pr
    - Adjust environment variables to account locator based (as currently
    there are a few ways of recognizing in which Snowflake environment we
    are)

--- pkg/acceptance/bettertestspoc/assert/resourceshowoutputassert/security_integration_show_output_ext.go
+++ pkg/acceptance/bettertestspoc/assert/resourceshowoutputassert/security_integration_show_output_ext.go
@@ -1,6 +1,8 @@
 package resourceshowoutputassert
 
-import "github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/acceptance/bettertestspoc/assert"
+import (
+	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/acceptance/bettertestspoc/assert"
+)
 
 func (s *SecurityIntegrationShowOutputAssert) HasCreatedOnNotEmpty() *SecurityIntegrationShowOutputAssert {
 	s.AddAssertion(assert.ResourceShowOutputValuePresent("created_on"))
