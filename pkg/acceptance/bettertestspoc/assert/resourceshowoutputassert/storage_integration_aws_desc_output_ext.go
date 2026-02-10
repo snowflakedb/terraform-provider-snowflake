82fab2e075754594a902da9a64dfdf0bcb1042b9
Artur Sawicki <artur.sawicki@snowflake.com>
2026-02-10 09:52:09 +0100

    Comment out assertions for sets temporarily

--- pkg/acceptance/bettertestspoc/assert/resourceshowoutputassert/storage_integration_aws_desc_output_ext.go
+++ pkg/acceptance/bettertestspoc/assert/resourceshowoutputassert/storage_integration_aws_desc_output_ext.go
@@ -1,7 +1,6 @@
 package resourceshowoutputassert
 
 import (
-	"fmt"
 	"strconv"
 
 	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/acceptance/bettertestspoc/assert"
@@ -20,16 +19,18 @@ func (s *StorageIntegrationAwsDescribeOutputAssert) HasExternalIdSet() *StorageI
 
 func (s *StorageIntegrationAwsDescribeOutputAssert) HasAllowedLocations(expected ...sdk.StorageLocation) *StorageIntegrationAwsDescribeOutputAssert {
 	s.AddAssertion(assert.ResourceDescribeOutputValueSet("allowed_locations.#", strconv.FormatInt(int64(len(expected)), 10)))
-	for i, v := range expected {
-		s.AddAssertion(assert.ResourceDescribeOutputValueSet(fmt.Sprintf("allowed_locations.%d", i), v.Path))
-	}
+	// TODO [next PRs]: these are sets so the order varies
+	// for i, v := range expected {
+	//	s.AddAssertion(assert.ResourceDescribeOutputValueSet(fmt.Sprintf("allowed_locations.%d", i), v.Path))
+	// }
 	return s
 }
 
 func (s *StorageIntegrationAwsDescribeOutputAssert) HasBlockedLocations(expected ...sdk.StorageLocation) *StorageIntegrationAwsDescribeOutputAssert {
 	s.AddAssertion(assert.ResourceDescribeOutputValueSet("blocked_locations.#", strconv.FormatInt(int64(len(expected)), 10)))
-	for i, v := range expected {
-		s.AddAssertion(assert.ResourceDescribeOutputValueSet(fmt.Sprintf("blocked_locations.%d", i), v.Path))
-	}
+	// TODO [next PRs]: these are sets so the order varies
+	// for i, v := range expected {
+	//	s.AddAssertion(assert.ResourceDescribeOutputValueSet(fmt.Sprintf("blocked_locations.%d", i), v.Path))
+	// }
 	return s
 }
