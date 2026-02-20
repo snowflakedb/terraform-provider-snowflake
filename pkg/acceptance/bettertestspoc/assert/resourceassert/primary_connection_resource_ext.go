1bac2c255fd6f39814930b78a6832b9dcb0e664b
Artur Sawicki <artur.sawicki@snowflake.com>
2026-02-20 16:07:10 +0100

    Add TODO

--- pkg/acceptance/bettertestspoc/assert/resourceassert/primary_connection_resource_ext.go
+++ pkg/acceptance/bettertestspoc/assert/resourceassert/primary_connection_resource_ext.go
@@ -5,6 +5,7 @@ import (
 	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/sdk"
 )
 
+// TODO [SNOW-3113138]: extract common conversions (like Name/FullyQualifiedName to string)
 func (c *PrimaryConnectionResourceAssert) HasExactlyFailoverToAccountsInOrder(expected ...sdk.AccountIdentifier) *PrimaryConnectionResourceAssert {
 	return c.HasEnableFailoverToAccounts(collections.Map(expected, func(v sdk.AccountIdentifier) string {
 		return v.Name()
