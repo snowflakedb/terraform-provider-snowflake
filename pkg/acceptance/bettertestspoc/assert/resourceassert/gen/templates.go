719b46dc5df86f617d3fc728f06ac6a999b02288
Artur Sawicki <artur.sawicki@snowflake.com>
2026-02-20 15:51:06 +0100

    Remove outdated comment

--- pkg/acceptance/bettertestspoc/assert/resourceassert/gen/templates.go
+++ pkg/acceptance/bettertestspoc/assert/resourceassert/gen/templates.go
@@ -13,7 +13,6 @@ var (
 	definitionTemplateContent string
 	DefinitionTemplate, _     = template.New("definitionTemplate").Parse(definitionTemplateContent)
 
-	// TODO [SNOW-3113128]: use .IsCollection logic for string checks
 	//go:embed templates/assertions.tmpl
 	assertionsTemplateContent string
 	AssertionsTemplate, _     = template.New("assertionsTemplate").Funcs(genhelpers.BuildTemplateFuncMap(
