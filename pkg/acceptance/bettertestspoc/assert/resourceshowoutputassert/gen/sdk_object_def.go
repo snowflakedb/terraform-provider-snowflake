fbd9f3d992a31947eaa9cc4daef693d8d5d9cd83
Artur Sawicki <artur.sawicki@snowflake.com>
2026-02-20 11:07:56 +0100

    Filter out non-resource objects for show output assertion generation

---
+++ pkg/acceptance/bettertestspoc/assert/resourceshowoutputassert/gen/sdk_object_def.go
@@ -0,0 +1,25 @@
+package gen
+
+import (
+	"reflect"
+	"slices"
+
+	objectassertgen "github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/acceptance/bettertestspoc/assert/objectassert/gen"
+
+	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/internal/collections"
+	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/internal/genhelpers"
+	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/sdk"
+)
+
+// GetFilteredSdkObjectDetails is currently needed to filter out objects that are not resources because the same underlying list of objects is used.
+func GetFilteredSdkObjectDetails() []genhelpers.SdkObjectDetails {
+	allDetails := objectassertgen.GetSdkObjectDetails()
+	return collections.Filter(allDetails, func(d genhelpers.SdkObjectDetails) bool {
+		return !slices.Contains(objectNamesNotBeingResources, d.Name)
+	})
+}
+
+var objectsNotBeingResources = []any{sdk.UserWorkloadIdentityAuthenticationMethod{}}
+var objectNamesNotBeingResources = collections.Map(objectsNotBeingResources, func(o any) string {
+	return reflect.ValueOf(o).Type().String()
+})
