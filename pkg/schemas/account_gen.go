025e68928624d6fa41839bb3bdb96ccc8cadaeb3
Jakub Michalak <jakub.michalak@snowflake.com>
2026-04-02 11:04:34 +0000

    Dereference optional fields

--- pkg/schemas/account_gen.go
+++ pkg/schemas/account_gen.go
@@ -135,79 +135,78 @@ func AccountToSchema(account *sdk.Account) map[string]any {
 	accountSchema["account_name"] = account.AccountName
 	accountSchema["snowflake_region"] = account.SnowflakeRegion
 	if account.RegionGroup != nil {
-		accountSchema["region_group"] = account.RegionGroup
+		accountSchema["region_group"] = (*account.RegionGroup)
 	}
 	if account.Edition != nil {
-		// Manually modified, please don't re-generate
-		accountSchema["edition"] = string(*account.Edition)
+		accountSchema["edition"] = string((*account.Edition))
 	}
 	if account.AccountURL != nil {
-		accountSchema["account_url"] = account.AccountURL
+		accountSchema["account_url"] = (*account.AccountURL)
 	}
 	if account.CreatedOn != nil {
-		accountSchema["created_on"] = account.CreatedOn.String()
+		accountSchema["created_on"] = (*account.CreatedOn).String()
 	}
 	if account.Comment != nil {
-		accountSchema["comment"] = account.Comment
+		accountSchema["comment"] = (*account.Comment)
 	}
 	accountSchema["account_locator"] = account.AccountLocator
 	if account.AccountLocatorUrl != nil {
-		accountSchema["account_locator_url"] = account.AccountLocatorUrl
+		accountSchema["account_locator_url"] = (*account.AccountLocatorUrl)
 	}
 	if account.ManagedAccounts != nil {
-		accountSchema["managed_accounts"] = account.ManagedAccounts
+		accountSchema["managed_accounts"] = (*account.ManagedAccounts)
 	}
 	if account.ConsumptionBillingEntityName != nil {
-		accountSchema["consumption_billing_entity_name"] = account.ConsumptionBillingEntityName
+		accountSchema["consumption_billing_entity_name"] = (*account.ConsumptionBillingEntityName)
 	}
 	if account.MarketplaceConsumerBillingEntityName != nil {
-		accountSchema["marketplace_consumer_billing_entity_name"] = account.MarketplaceConsumerBillingEntityName
+		accountSchema["marketplace_consumer_billing_entity_name"] = (*account.MarketplaceConsumerBillingEntityName)
 	}
 	if account.MarketplaceProviderBillingEntityName != nil {
-		accountSchema["marketplace_provider_billing_entity_name"] = account.MarketplaceProviderBillingEntityName
+		accountSchema["marketplace_provider_billing_entity_name"] = (*account.MarketplaceProviderBillingEntityName)
 	}
 	if account.OldAccountURL != nil {
-		accountSchema["old_account_url"] = account.OldAccountURL
+		accountSchema["old_account_url"] = (*account.OldAccountURL)
 	}
 	if account.IsOrgAdmin != nil {
-		accountSchema["is_org_admin"] = account.IsOrgAdmin
+		accountSchema["is_org_admin"] = (*account.IsOrgAdmin)
 	}
 	if account.AccountOldUrlSavedOn != nil {
-		accountSchema["account_old_url_saved_on"] = account.AccountOldUrlSavedOn.String()
+		accountSchema["account_old_url_saved_on"] = (*account.AccountOldUrlSavedOn).String()
 	}
 	if account.AccountOldUrlLastUsed != nil {
-		accountSchema["account_old_url_last_used"] = account.AccountOldUrlLastUsed.String()
+		accountSchema["account_old_url_last_used"] = (*account.AccountOldUrlLastUsed).String()
 	}
 	if account.OrganizationOldUrl != nil {
-		accountSchema["organization_old_url"] = account.OrganizationOldUrl
+		accountSchema["organization_old_url"] = (*account.OrganizationOldUrl)
 	}
 	if account.OrganizationOldUrlSavedOn != nil {
-		accountSchema["organization_old_url_saved_on"] = account.OrganizationOldUrlSavedOn.String()
+		accountSchema["organization_old_url_saved_on"] = (*account.OrganizationOldUrlSavedOn).String()
 	}
 	if account.OrganizationOldUrlLastUsed != nil {
-		accountSchema["organization_old_url_last_used"] = account.OrganizationOldUrlLastUsed.String()
+		accountSchema["organization_old_url_last_used"] = (*account.OrganizationOldUrlLastUsed).String()
 	}
 	if account.IsEventsAccount != nil {
-		accountSchema["is_events_account"] = account.IsEventsAccount
+		accountSchema["is_events_account"] = (*account.IsEventsAccount)
 	}
 	accountSchema["is_organization_account"] = account.IsOrganizationAccount
 	if account.DroppedOn != nil {
-		accountSchema["dropped_on"] = account.DroppedOn.String()
+		accountSchema["dropped_on"] = (*account.DroppedOn).String()
 	}
 	if account.ScheduledDeletionTime != nil {
-		accountSchema["scheduled_deletion_time"] = account.ScheduledDeletionTime.String()
+		accountSchema["scheduled_deletion_time"] = (*account.ScheduledDeletionTime).String()
 	}
 	if account.RestoredOn != nil {
-		accountSchema["restored_on"] = account.RestoredOn.String()
+		accountSchema["restored_on"] = (*account.RestoredOn).String()
 	}
 	if account.MovedToOrganization != nil {
-		accountSchema["moved_to_organization"] = account.MovedToOrganization
+		accountSchema["moved_to_organization"] = (*account.MovedToOrganization)
 	}
 	if account.MovedOn != nil {
-		accountSchema["moved_on"] = account.MovedOn
+		accountSchema["moved_on"] = (*account.MovedOn)
 	}
 	if account.OrganizationUrlExpirationOn != nil {
-		accountSchema["organization_url_expiration_on"] = account.OrganizationUrlExpirationOn.String()
+		accountSchema["organization_url_expiration_on"] = (*account.OrganizationUrlExpirationOn).String()
 	}
 	return accountSchema
 }
