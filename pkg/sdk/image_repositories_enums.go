c8af5e097e5884a1dd213ed93adf0cf268ff676f
Artur Sawicki <artur.sawicki@snowflake.com>
2026-04-09 01:16:46 +0200

    Improve encryption type enum

--- pkg/sdk/image_repositories_enums.go
+++ pkg/sdk/image_repositories_enums.go
@@ -1,8 +1,27 @@
 package sdk
 
+import (
+	"fmt"
+	"slices"
+	"strings"
+)
+
 type ImageRepositoryEncryptionType string
 
-var (
+const (
 	ImageRepositoryEncryptionTypeSnowflakeFull ImageRepositoryEncryptionType = "SNOWFLAKE_FULL"
 	ImageRepositoryEncryptionTypeSnowflakeSse  ImageRepositoryEncryptionType = "SNOWFLAKE_SSE"
 )
+
+var AllImageRepositoryEncryptionTypes = []ImageRepositoryEncryptionType{
+	ImageRepositoryEncryptionTypeSnowflakeFull,
+	ImageRepositoryEncryptionTypeSnowflakeSse,
+}
+
+func ToImageRepositoryEncryptionType(s string) (ImageRepositoryEncryptionType, error) {
+	s = strings.ToUpper(s)
+	if !slices.Contains(AllImageRepositoryEncryptionTypes, ImageRepositoryEncryptionType(s)) {
+		return "", fmt.Errorf("invalid image repository encryption type: %s", s)
+	}
+	return ImageRepositoryEncryptionType(s), nil
+}
