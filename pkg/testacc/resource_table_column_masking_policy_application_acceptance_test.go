395d397f8c2f14594c43c814e4332ce3adae2647
Artur Sawicki <artur.sawicki@snowflake.com>
2026-04-08 15:30:37 +0200

    Fix describe output parsing in table column masking policy application
    References: #4608

--- pkg/testacc/resource_table_column_masking_policy_application_acceptance_test.go
+++ pkg/testacc/resource_table_column_masking_policy_application_acceptance_test.go
@@ -4,6 +4,7 @@ package testacc
 
 import (
 	"fmt"
+	"regexp"
 	"testing"
 
 	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/sdk"
@@ -72,4 +73,37 @@ resource "snowflake_table_column_masking_policy_application" "mpa" {
 	column         = "secret"
 	masking_policy = snowflake_masking_policy.test.fully_qualified_name
 }`, tableId.DatabaseName(), tableId.SchemaName(), tableId.Name(), maskingPolicyId.Name())
+}
+
+// proves the fix for https://github.com/snowflakedb/terraform-provider-snowflake/issues/4608
+func TestAcc_TableColumnMaskingPolicyApplication_BCR2026_02(t *testing.T) {
+	tableId := testClient().Ids.RandomSchemaObjectIdentifier()
+	maskingPolicyId := testClient().Ids.RandomSchemaObjectIdentifier()
+
+	resource.Test(t, resource.TestCase{
+		TerraformVersionChecks: []tfversion.TerraformVersionCheck{
+			tfversion.RequireAbove(tfversion.Version1_5_0),
+		},
+		Steps: []resource.TestStep{
+			{
+				ExternalProviders: ExternalProviderWithExactVersion("2.14.1"),
+				Config:            maskingPolicyApplicationBcr202602TestConfig(tableId, maskingPolicyId),
+				ExpectError:       regexp.MustCompile("expected 13 destination arguments in Scan, not 12"),
+			},
+			{
+				ProtoV6ProviderFactories: TestAccProtoV6ProviderFactories,
+				Config:                   maskingPolicyApplicationTestConfig(tableId, maskingPolicyId),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("snowflake_table_column_masking_policy_application.mpa", "table", tableId.FullyQualifiedName()),
+				),
+			},
+		},
+	})
+}
+
+func maskingPolicyApplicationBcr202602TestConfig(tableId sdk.SchemaObjectIdentifier, maskingPolicyId sdk.SchemaObjectIdentifier) string {
+	return `provider "snowflake" {
+  preview_features_enabled = ["snowflake_table_resource", "snowflake_table_column_masking_policy_application_resource"]
+}
+` + maskingPolicyApplicationTestConfig(tableId, maskingPolicyId)
 }
