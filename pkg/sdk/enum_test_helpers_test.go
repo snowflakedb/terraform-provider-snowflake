3fc36272617e04ca6d9816932fd63a71546dd20b
Artur Sawicki <artur.sawicki@snowflake.com>
2026-04-09 23:36:37 +0200

    Inline the helper method

--- pkg/sdk/enum_test_helpers_test.go
+++ pkg/sdk/enum_test_helpers_test.go
@@ -18,8 +18,37 @@ type typedEnumTestProvider[T ~string] struct {
 	conversionFunc func(string) (T, error)
 }
 
+// RunTest is a generic helper that verifies a To<EnumName> conversion function
+// against all enum values (currently without aliases).
+// TODO [SNOW-2324252]: support aliases
 func (p typedEnumTestProvider[T]) RunTest(t *testing.T) {
-	testEnumConversion(t, p.enumName, p.allValues, p.conversionFunc)
+	t.Helper()
+
+	t.Run(fmt.Sprintf("%s case-insensitive", p.enumName), func(t *testing.T) {
+		require.NotEmpty(t, p.allValues, "allValidValues must not be empty")
+
+		got, err := p.conversionFunc(strings.ToLower(string(p.allValues[0])))
+
+		require.NoError(t, err)
+		require.Equal(t, p.allValues[0], got)
+	})
+
+	for _, v := range p.allValues {
+		t.Run(fmt.Sprintf("%s: %s", p.enumName, v), func(t *testing.T) {
+			got, err := p.conversionFunc(string(v))
+
+			require.NoError(t, err)
+			require.Equal(t, v, got)
+		})
+	}
+
+	for _, v := range []string{"", "foo"} {
+		t.Run(fmt.Sprintf("%s invalid: %s", p.enumName, v), func(t *testing.T) {
+			_, err := p.conversionFunc(v)
+
+			require.Error(t, err)
+		})
+	}
 }
 
 func Test_AllEnumConversions(t *testing.T) {
@@ -29,38 +58,5 @@ func Test_AllEnumConversions(t *testing.T) {
 
 	for _, tp := range all {
 		tp.RunTest(t)
-	}
-}
-
-// testEnumConversion is a generic helper that verifies a To<EnumName> conversion function
-// against all enum values (currently without aliases).
-// TODO [SNOW-2324252]: support aliases
-func testEnumConversion[T ~string](t *testing.T, enumName string, allValues []T, convert func(string) (T, error)) {
-	t.Helper()
-
-	t.Run(fmt.Sprintf("%s case-insensitive", enumName), func(t *testing.T) {
-		require.NotEmpty(t, allValues, "allValidValues must not be empty")
-
-		got, err := convert(strings.ToLower(string(allValues[0])))
-
-		require.NoError(t, err)
-		require.Equal(t, allValues[0], got)
-	})
-
-	for _, v := range allValues {
-		t.Run(fmt.Sprintf("%s: %s", enumName, v), func(t *testing.T) {
-			got, err := convert(string(v))
-
-			require.NoError(t, err)
-			require.Equal(t, v, got)
-		})
-	}
-
-	for _, v := range []string{"", "foo"} {
-		t.Run(fmt.Sprintf("%s invalid: %s", enumName, v), func(t *testing.T) {
-			_, err := convert(v)
-
-			require.Error(t, err)
-		})
 	}
 }
