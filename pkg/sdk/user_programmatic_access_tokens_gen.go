45ac8ea15cf6d9a14f7aa17583eb976633ba28bd
Artur Sawicki <artur.sawicki@snowflake.com>
2026-04-17 16:52:55 +0200

    Adjust user programmatic access token

--- pkg/sdk/user_programmatic_access_tokens_gen.go
+++ pkg/sdk/user_programmatic_access_tokens_gen.go
@@ -14,9 +14,9 @@ type UserProgrammaticAccessTokens interface {
 	Rotate(ctx context.Context, request *RotateUserProgrammaticAccessTokenRequest) (*RotateProgrammaticAccessTokenResult, error)
 	Remove(ctx context.Context, request *RemoveUserProgrammaticAccessTokenRequest) error
 	Show(ctx context.Context, request *ShowUserProgrammaticAccessTokenRequest) ([]ProgrammaticAccessToken, error)
-	// adjusted manually
+	// adjusted manually - adding userId
 	ShowByID(ctx context.Context, userId, id AccountObjectIdentifier) (*ProgrammaticAccessToken, error)
-	// adjusted manually
+	// adjusted manually - adding userId
 	ShowByIDSafely(ctx context.Context, userId, id AccountObjectIdentifier) (*ProgrammaticAccessToken, error)
 	RemoveByIDSafely(ctx context.Context, request *RemoveUserProgrammaticAccessTokenRequest) error
 }
