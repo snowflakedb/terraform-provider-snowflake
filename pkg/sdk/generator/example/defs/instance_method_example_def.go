dfb29635955bf4180bb93c4648cec8869ed04284
Artur Sawicki <artur.sawicki@snowflake.com>
2026-04-26 19:08:42 +0200

    Add example

---
+++ pkg/sdk/generator/example/defs/instance_method_example_def.go
@@ -0,0 +1,31 @@
+package defs
+
+import (
+	g "github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/sdk/generator/gen"
+	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/sdk/generator/gen/sdkcommons"
+)
+
+var addNotificationIntegrationArgs = g.NewQueryStruct("AddNotificationIntegrationArgs").
+	Identifier("IntegrationName", g.KindOfT[sdkcommons.AccountObjectIdentifier](), g.IdentifierOptions()).
+	WithValidation(g.ValidIdentifier, "IntegrationName")
+
+var addNotificationIntegrationResult = g.StructPair(
+	"addNotificationIntegrationRow",
+	"AddNotificationIntegrationResult",
+).Text("status")
+
+var InstanceMethodExamplesDef = g.NewInterface(
+	"InstanceMethodExamples",
+	"InstanceMethodExample",
+	g.KindOfT[sdkcommons.AccountObjectIdentifier](),
+).InstanceMethodOperation(
+	"https://docs.snowflake.com/en/sql-reference/classes/budget/method/add_notification_integration",
+	"ADD_NOTIFICATION_INTEGRATION",
+	addNotificationIntegrationArgs,
+	addNotificationIntegrationResult,
+).InstanceMethodOperation(
+	"https://docs.snowflake.com/en/sql-reference/classes/budget/method/get_spending_limit",
+	"GET_SPENDING_LIMIT",
+	nil,
+	nil,
+)
