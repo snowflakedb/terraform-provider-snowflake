99a1998cd434170b92785aef7ca6c7398da8aa78
James Southard <james.southard@snowflake.com>
2026-04-29 20:31:57 -0400

    feat: adds flat struct for postgres instance details

--- pkg/sdk/postgres_instances_ext.go
+++ pkg/sdk/postgres_instances_ext.go
@@ -1,7 +1,89 @@
 package sdk
 
+import "strconv"
+
 func (r *CreatePostgresInstanceRequest) GetName() AccountObjectIdentifier {
 	return r.name
+}
+
+// PostgresInstanceDetails represents the parsed result of DESCRIBE POSTGRES INSTANCE
+type PostgresInstanceDetails struct {
+	Name                         string
+	Owner                        string
+	OwnerRoleType                string
+	CreatedOn                    string
+	UpdatedOn                    string
+	Type                         string
+	Host                         string
+	Origin                       *string
+	PrivatelinkServiceIdentifier *string
+	ComputeFamily                string
+	StorageSizeGb                int
+	PostgresVersion              string
+	HighAvailability             bool
+	AuthenticationAuthority      string
+	State                        string
+	RetentionTime                int
+	MaintenanceWindowStart       int
+	Comment                      *string
+	NetworkPolicy                *string
+	PostgresSettings             *string
+	StorageIntegration           *string
+}
+
+// ParsePostgresInstanceDetails parses []PostgresInstanceProperty into PostgresInstanceDetails
+func ParsePostgresInstanceDetails(properties []PostgresInstanceProperty) (*PostgresInstanceDetails, error) {
+	details := &PostgresInstanceDetails{}
+	for _, prop := range properties {
+		switch prop.Property {
+		case "name":
+			details.Name = prop.Value
+		case "owner":
+			details.Owner = prop.Value
+		case "owner_role_type":
+			details.OwnerRoleType = prop.Value
+		case "created_on":
+			details.CreatedOn = prop.Value
+		case "updated_on":
+			details.UpdatedOn = prop.Value
+		case "type":
+			details.Type = prop.Value
+		case "host":
+			details.Host = prop.Value
+		case "origin":
+			details.Origin = String(prop.Value)
+		case "privatelink_service_identifier":
+			details.PrivatelinkServiceIdentifier = String(prop.Value)
+		case "compute_family":
+			details.ComputeFamily = prop.Value
+		case "storage_size_gb":
+			val, _ := strconv.Atoi(prop.Value)
+			details.StorageSizeGb = val
+		case "postgres_version":
+			details.PostgresVersion = prop.Value
+		case "high_availability":
+			details.HighAvailability = prop.Value == "true"
+		case "authentication_authority":
+			details.AuthenticationAuthority = prop.Value
+		case "state":
+			details.State = prop.Value
+		case "retention_time":
+			val, _ := strconv.Atoi(prop.Value)
+			details.RetentionTime = val
+		case "maintenance_window_start":
+			val, _ := strconv.Atoi(prop.Value)
+			details.MaintenanceWindowStart = val
+		case "comment":
+			details.Comment = String(prop.Value)
+		case "network_policy":
+			details.NetworkPolicy = String(prop.Value)
+		case "postgres_settings":
+			details.PostgresSettings = String(prop.Value)
+		case "storage_integration":
+			details.StorageIntegration = String(prop.Value)
+		}
+	}
+	return details, nil
 }
 
 func (r postgresInstancesRow) convert() (*PostgresInstance, error) {
