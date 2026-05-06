5fc8d9966b65d89d40402cbeb6aafa14676fea28
Jan Cieślak <jan.cieslak@snowflake.com>
2026-05-06 09:56:10 +0200

    Revert "wip"
    This reverts commit 42a83e74

--- pkg/acceptance/bettertestspoc/assert/objectassert/tag_snowflake_ext.go
+++ pkg/acceptance/bettertestspoc/assert/objectassert/tag_snowflake_ext.go
@@ -56,31 +56,3 @@ func (t *TagAssert) HasAllowedValuesEmpty() *TagAssert {
 func (t *TagAssert) HasPropagateEnum(expected sdk.TagPropagation) *TagAssert {
 	return t.HasPropagate(string(expected))
 }
-
-// HasOnConflict asserts that the tag's OnConflict value matches the expected string.
-// This is only populated when BCR-2291 is enabled.
-func (t *TagAssert) HasOnConflict(expected string) *TagAssert {
-	t.AddAssertion(func(t *testing.T, o *sdk.Tag) error {
-		t.Helper()
-		if o.OnConflict == nil {
-			return fmt.Errorf("expected on_conflict: %q; got nil (BCR-2291 may not be enabled)", expected)
-		}
-		if *o.OnConflict != expected {
-			return fmt.Errorf("expected on_conflict: %q; got: %q", expected, *o.OnConflict)
-		}
-		return nil
-	})
-	return t
-}
-
-// HasOnConflictNil asserts that the tag's OnConflict field is nil (BCR-2291 not enabled or on_conflict not set).
-func (t *TagAssert) HasOnConflictNil() *TagAssert {
-	t.AddAssertion(func(t *testing.T, o *sdk.Tag) error {
-		t.Helper()
-		if o.OnConflict != nil {
-			return fmt.Errorf("expected on_conflict to be nil; got: %q", *o.OnConflict)
-		}
-		return nil
-	})
-	return t
-}
