402b437447a3a1db7097382df0960374a4c5cfaa
Jan Cieślak <jan.cieslak@snowflake.com>
2026-05-15 15:15:42 +0200

    Adjust

--- pkg/sdk/testint/tags_account_level_integration_test.go
+++ pkg/sdk/testint/tags_account_level_integration_test.go
@@ -5,6 +5,7 @@ package testint
 import (
 	"testing"
 
+	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/acceptance/bettertestspoc/assert/objectassert"
 	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/sdk"
 	"github.com/stretchr/testify/require"
 )
@@ -40,5 +41,88 @@ func TestInt_TagsAccountLevel(t *testing.T) {
 		assertTagUnset(t, tag.ID(), id, sdk.ObjectTypeIntegration)
 
 		testSetAndUnsetInTagObject(t, tags[0], id, sdk.ObjectTypeIntegration)
+	})
+}
+
+// TestInt_Tags_OnConflict_Bcr2291 verifies that the on_conflict field added to SHOW TAGS by BCR-2291
+// (bundle 2026_03) is correctly parsed and surfaced through ShowByID.
+func TestInt_Tags_OnConflict_Bcr2291(t *testing.T) {
+	t.Run("bundle enabled: create with custom_value is returned by ShowByID", func(t *testing.T) {
+		tag, tagCleanup := testClientHelper().Tag.CreateWithRequest(t,
+			sdk.NewCreateTagRequest(testClientHelper().Ids.RandomSchemaObjectIdentifier()).
+				WithPropagate(*sdk.NewTagPropagateRequest(sdk.TagPropagationOnDependency).
+					WithOnConflict(sdk.TagOnConflict{CustomValue: sdk.String("HIGHLY CONFIDENTIAL")})),
+		)
+		t.Cleanup(tagCleanup)
+
+		assertThatObject(t, objectassert.Tag(t, tag.ID()).
+			HasOnConflict("HIGHLY CONFIDENTIAL"),
+		)
+	})
+
+	t.Run("bundle enabled: create with allowed_values_sequence is returned by ShowByID", func(t *testing.T) {
+		tag, tagCleanup := testClientHelper().Tag.CreateWithRequest(t,
+			sdk.NewCreateTagRequest(testClientHelper().Ids.RandomSchemaObjectIdentifier()).
+				WithAllowedValues([]string{"confidential", "internal", "public"}).
+				WithPropagate(*sdk.NewTagPropagateRequest(sdk.TagPropagationOnDependency).
+					WithOnConflict(sdk.TagOnConflict{AllowedValuesSequence: sdk.Bool(true)})),
+		)
+		t.Cleanup(tagCleanup)
+
+		assertThatObject(t, objectassert.Tag(t, tag.ID()).
+			HasOnConflict("ALLOWED_VALUES_SEQUENCE"),
+		)
+	})
+
+	t.Run("bundle enabled: alter on_conflict custom value", func(t *testing.T) {
+		tag, tagCleanup := testClientHelper().Tag.CreateWithRequest(t,
+			sdk.NewCreateTagRequest(testClientHelper().Ids.RandomSchemaObjectIdentifier()).
+				WithPropagate(*sdk.NewTagPropagateRequest(sdk.TagPropagationOnDependency)),
+		)
+		t.Cleanup(tagCleanup)
+
+		assertThatObject(t, objectassert.Tag(t, tag.ID()).HasOnConflictNil())
+
+		testClientHelper().Tag.Alter(t, sdk.NewAlterTagRequest(tag.ID()).
+			WithSet(*sdk.NewTagSetRequest().WithPropagate(*sdk.NewTagPropagateRequest(sdk.TagPropagationOnDependency).
+				WithOnConflict(sdk.TagOnConflict{CustomValue: sdk.String("my_custom_value")}))))
+
+		assertThatObject(t, objectassert.Tag(t, tag.ID()).
+			HasOnConflict("my_custom_value"),
+		)
+
+		testClientHelper().Tag.Alter(t, sdk.NewAlterTagRequest(tag.ID()).
+			WithUnset(*sdk.NewTagUnsetRequest().WithPropagate(true)))
+
+		assertThatObject(t, objectassert.Tag(t, tag.ID()).HasOnConflictNil())
+	})
+
+	t.Run("bundle enabled: alter on_conflict allowed values sequence", func(t *testing.T) {
+		tag, tagCleanup := testClientHelper().Tag.CreateWithRequest(t,
+			sdk.NewCreateTagRequest(testClientHelper().Ids.RandomSchemaObjectIdentifier()).
+				WithPropagate(*sdk.NewTagPropagateRequest(sdk.TagPropagationOnDependency).
+					WithOnConflict(sdk.TagOnConflict{CustomValue: sdk.String("will_be_cleared")})),
+		)
+		t.Cleanup(tagCleanup)
+
+		assertThatObject(t, objectassert.Tag(t, tag.ID()).HasOnConflict("will_be_cleared"))
+
+		testClientHelper().Tag.Alter(t, sdk.NewAlterTagRequest(tag.ID()).
+			WithUnset(*sdk.NewTagUnsetRequest().WithPropagate(true)))
+
+		assertThatObject(t, objectassert.Tag(t, tag.ID()).HasOnConflictNil())
+	})
+
+	t.Run("bundle disabled: on_conflict is nil in ShowByID even when set", func(t *testing.T) {
+		testClientHelper().BcrBundles.DisableBcrBundle(t, "2026_03")
+
+		tag, tagCleanup := testClientHelper().Tag.CreateWithRequest(t,
+			sdk.NewCreateTagRequest(testClientHelper().Ids.RandomSchemaObjectIdentifier()).
+				WithPropagate(*sdk.NewTagPropagateRequest(sdk.TagPropagationOnDependency).
+					WithOnConflict(sdk.TagOnConflict{CustomValue: sdk.String("will_not_appear")})),
+		)
+		t.Cleanup(tagCleanup)
+
+		assertThatObject(t, objectassert.Tag(t, tag.ID()).HasOnConflictNil())
 	})
 }
