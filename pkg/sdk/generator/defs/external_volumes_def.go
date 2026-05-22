03cc22a72b4a81a09c8dde73bf69bc21ea67474a
Artur Sawicki <artur.sawicki@snowflake.com>
2026-05-22 18:34:28 +0200

    Generate converts for external volumes

--- pkg/sdk/generator/defs/external_volumes_def.go
+++ pkg/sdk/generator/defs/external_volumes_def.go
@@ -183,7 +183,8 @@ var externalVolumesDef = g.NewInterface(
 			Text("property", g.WithPlainFieldName("Name")).
 			Text("property_type", g.WithPlainFieldName("Type")).
 			Text("property_value", g.WithPlainFieldName("Value")).
-			Text("property_default", g.WithPlainFieldName("Default")),
+			Text("property_default", g.WithPlainFieldName("Default")).
+			WithConvertGeneration(),
 		g.NewQueryStruct("DescExternalVolume").
 			Describe().
 			SQL("EXTERNAL VOLUME").
@@ -195,7 +196,8 @@ var externalVolumesDef = g.NewInterface(
 		g.StructPair("externalVolumeShowRow", "ExternalVolume").
 			Text("name").
 			Bool("allow_writes").
-			OptionalText("comment", g.WithRequiredInPlain()),
+			OptionalText("comment", g.WithRequiredInPlain()).
+			WithConvertGeneration(),
 		g.NewQueryStruct("ShowExternalVolumes").
 			Show().
 			SQL("EXTERNAL VOLUMES").
