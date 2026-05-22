5ff586c6eb76a68a7f726010b2cfd216d9b1882d
Artur Sawicki <artur.sawicki@snowflake.com>
2026-05-22 17:27:44 +0200

    Adjust views validation

--- pkg/sdk/views_ext.go
+++ pkg/sdk/views_ext.go
@@ -1,6 +1,42 @@
 package sdk
 
-import "strings"
+import (
+	"fmt"
+	"strings"
+)
+
+func (p *ViewRowAccessPolicy) additionalValidations() error {
+	if !valueSet(p.On) {
+		return errNotSet("CreateViewOptions.RowAccessPolicy", "On")
+	}
+	return nil
+}
+
+func (p *ViewAddRowAccessPolicy) additionalValidations() error {
+	if !valueSet(p.On) {
+		return errNotSet("AlterViewOptions.AddRowAccessPolicy", "On")
+	}
+	return nil
+}
+
+func (opts *CreateViewOptions) additionalValidations() error {
+	var errs []error
+	if valueSet(opts.Columns) {
+		for i, columnOption := range opts.Columns {
+			if valueSet(columnOption.MaskingPolicy) {
+				if !ValidObjectIdentifier(columnOption.MaskingPolicy.MaskingPolicy) {
+					errs = append(errs, errInvalidIdentifier(fmt.Sprintf("CreateViewOptions.Columns[%d]", i), "MaskingPolicy"))
+				}
+			}
+			if valueSet(columnOption.ProjectionPolicy) {
+				if !ValidObjectIdentifier(columnOption.ProjectionPolicy.ProjectionPolicy) {
+					errs = append(errs, errInvalidIdentifier(fmt.Sprintf("CreateViewOptions.Columns[%d]", i), "ProjectionPolicy"))
+				}
+			}
+		}
+	}
+	return JoinErrors(errs...)
+}
 
 var AllViewDataMetricScheduleMinutes = []int{5, 15, 30, 60, 720, 1440}
 
