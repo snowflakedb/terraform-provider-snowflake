992e9af5b387c409fad43875d4f873fb092e88e7
Artur Sawicki <artur.sawicki@snowflake.com>
2026-05-25 21:25:14 +0200

    Run for stages

--- pkg/sdk/generator/defs/stages_def.go
+++ pkg/sdk/generator/defs/stages_def.go
@@ -52,7 +52,8 @@ func createStageOperation(structName string, apply func(qs *g.QueryStruct) *g.Qu
 			g.NewQueryStruct("StageFileFormat").
 				OptionalIdentifier("FormatName", g.KindOfT[sdkcommons.SchemaObjectIdentifier](), g.IdentifierOptions().SQL("FORMAT_NAME =")).
 				PredefinedQueryStructField("FileFormatOptions", "*FileFormatOptions", g.ListOptions()).
-				WithValidation(g.ExactlyOneValueSet, "FormatName", "FileFormatOptions"),
+				WithValidation(g.ExactlyOneValueSet, "FormatName", "FileFormatOptions").
+				WithAdditionalValidations(),
 			g.ListOptions().Parentheses().NoComma().SQL("FILE_FORMAT ="),
 		).
 		OptionalComment().
@@ -74,7 +75,8 @@ func alterStageOperation(structName string, apply func(qs *g.QueryStruct) *g.Que
 			g.NewQueryStruct("StageFileFormat").
 				OptionalIdentifier("FormatName", g.KindOfT[sdkcommons.SchemaObjectIdentifier](), g.IdentifierOptions().SQL("FORMAT_NAME =")).
 				PredefinedQueryStructField("FileFormatOptions", "*FileFormatOptions", g.ListOptions()).
-				WithValidation(g.ExactlyOneValueSet, "FormatName", "FileFormatOptions"),
+				WithValidation(g.ExactlyOneValueSet, "FormatName", "FileFormatOptions").
+				WithAdditionalValidations(),
 			g.ListOptions().Parentheses().NoComma().SQL("FILE_FORMAT ="),
 		).
 		// TODO(SNOW-3035788): use COMMENT in unset and here use OptionalComment
