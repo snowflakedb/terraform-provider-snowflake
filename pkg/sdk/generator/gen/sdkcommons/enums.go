831e894a7663df423b3740e7f6c7be5f623f5adc
Jan Cieślak <jan.cieslak@snowflake.com>
2026-05-25 10:44:14 +0200

    run pre-push

--- pkg/sdk/generator/gen/sdkcommons/enums.go
+++ pkg/sdk/generator/gen/sdkcommons/enums.go
@@ -1,8 +1,8 @@
 package sdkcommons
 
 type (
-	ColumnConstraintType string
-	AutoEventLogging     string
+	ColumnConstraintType                                     string
+	AutoEventLogging                                         string
 	ComputePoolInstanceFamily                                string
 	DataType                                                 string
 	ImageRepositoryEncryptionType                            string
