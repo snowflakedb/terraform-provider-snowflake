1984d88e45bdef17a53d93318353d4f9e4d3c59d
Artur Sawicki <artur.sawicki@snowflake.com>
2026-06-01 18:40:44 +0200

    Regenerate after template change

--- pkg/sdk/row_access_policies_impl_gen.go
+++ pkg/sdk/row_access_policies_impl_gen.go
@@ -153,10 +153,10 @@ func (r describeRowAccessPolicyDBRow) convert() (*RowAccessPolicyDescription, er
 		ReturnType: r.ReturnType,
 		Body:       r.Body,
 	}
-	if v, err := ParseTableColumnSignature(r.Signature); err == nil {
-		result.Signature = v
-	} else {
+	if v, err := ParseTableColumnSignature(r.Signature); err != nil {
 		return nil, fmt.Errorf("parsing table column signature: %w", err)
+	} else {
+		result.Signature = v
 	}
 	return result, nil
 }
