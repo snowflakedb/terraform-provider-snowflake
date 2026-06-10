bcd3cc3bb2d521a96ab271b85d6b1483137e5442
Jan Cieślak <jan.cieslak@snowflake.com>
2026-06-10 15:13:59 +0200

    merge with dev

--- pkg/provider/previewfeatures/preview_features_test.go
+++ pkg/provider/previewfeatures/preview_features_test.go
@@ -24,6 +24,7 @@ func Test_StringToFeature(t *testing.T) {
 		{input: "snowflake_alert_resource", want: AlertResource},
 		{input: "snowflake_alerts_datasource", want: AlertsDatasource},
 		{input: "snowflake_api_integration_resource", want: ApiIntegrationResource},
+		{input: "snowflake_api_integration_amazon_api_gateway_resource", want: ApiIntegrationAmazonApiGatewayResource},
 		{input: "snowflake_authentication_policy_resource", want: AuthenticationPolicyResource},
 		{input: "snowflake_authentication_policies_datasource", want: AuthenticationPoliciesDatasource},
 		{input: "snowflake_catalog_integration_aws_glue_resource", want: CatalogIntegrationAwsGlueResource},
