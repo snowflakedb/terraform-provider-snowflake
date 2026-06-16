70b74230a76721e55251b96508253381d7137138
Kamil Wasilewski <kamil.wasilewski@snowflake.com>
2026-06-16 15:15:32 +0200

    Revert "add acceptance tests"
    This reverts commit 451e90216e196aace7577df473e516524ad68860.

--- pkg/testacc/testdata/TestAcc_GrantOwnership/OnAgent_ToAccountRole/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "account_role_name" {
-  type = string
-}
-
-variable "cortex_agent_fully_qualified_name" {
-  type = string
-}
--- pkg/testacc/testdata/TestAcc_GrantOwnership/OnAllAgents_ToAccountRole/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "account_role_name" {
-  type = string
-}
-
-variable "schema_fully_qualified_name" {
-  type = string
-}
