024a7feb5422a86bfc19fafb089a4ae6dcb24a38
Artur Sawicki <artur.sawicki@snowflake.com>
2026-06-25 12:52:54 +0200

    Fix ext for database

--- pkg/acceptance/bettertestspoc/assert/resourceshowoutputassert/database_show_output_ext.go
+++ pkg/acceptance/bettertestspoc/assert/resourceshowoutputassert/database_show_output_ext.go
@@ -1,35 +1,31 @@
 package resourceshowoutputassert
 
-import (
-	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/acceptance/bettertestspoc/assert"
-)
-
 func (s *DatabaseShowOutputAssert) HasCreatedOnNotEmpty() *DatabaseShowOutputAssert {
-	s.AddAssertion(assert.ResourceShowOutputValuePresent("created_on"))
+	s.ValuePresent("created_on")
 	return s
 }
 
 func (s *DatabaseShowOutputAssert) HasIsCurrentNotEmpty() *DatabaseShowOutputAssert {
-	s.AddAssertion(assert.ResourceShowOutputValuePresent("is_current"))
+	s.ValuePresent("is_current")
 	return s
 }
 
 func (s *DatabaseShowOutputAssert) HasOwnerNotEmpty() *DatabaseShowOutputAssert {
-	s.AddAssertion(assert.ResourceShowOutputValuePresent("owner"))
+	s.ValuePresent("owner")
 	return s
 }
 
 func (s *DatabaseShowOutputAssert) HasRetentionTimeNotEmpty() *DatabaseShowOutputAssert {
-	s.AddAssertion(assert.ResourceShowOutputValuePresent("retention_time"))
+	s.ValuePresent("retention_time")
 	return s
 }
 
 func (s *DatabaseShowOutputAssert) HasOwnerRoleTypeNotEmpty() *DatabaseShowOutputAssert {
-	s.AddAssertion(assert.ResourceShowOutputValuePresent("owner_role_type"))
+	s.ValuePresent("owner_role_type")
 	return s
 }
 
 func (s *DatabaseShowOutputAssert) HasOriginEmpty() *DatabaseShowOutputAssert {
-	s.AddAssertion(assert.ResourceShowOutputValueSet("origin", ""))
+	s.ValueSet("origin", "")
 	return s
 }
