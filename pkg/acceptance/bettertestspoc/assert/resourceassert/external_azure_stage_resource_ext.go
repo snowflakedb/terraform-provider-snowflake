861bb26e850b6734fb1377316da58cb1ae492669
Artur Sawicki <artur.sawicki@snowflake.com>
2026-06-26 14:58:42 +0200

    Regenerate external azure stage resource assertions

--- pkg/acceptance/bettertestspoc/assert/resourceassert/external_azure_stage_resource_ext.go
+++ pkg/acceptance/bettertestspoc/assert/resourceassert/external_azure_stage_resource_ext.go
@@ -3,7 +3,6 @@ package resourceassert
 import (
 	"strconv"
 
-	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/acceptance/bettertestspoc/assert"
 	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/sdk"
 )
 
@@ -27,54 +26,50 @@ func (e *ExternalAzureStageResourceAssert) HasDirectory(opts ExternalStageDirect
 	if opts.AutoRefresh != nil {
 		autoRefresh = *opts.AutoRefresh
 	}
-	e.AddAssertion(assert.ValueSet("directory.#", "1"))
-	e.AddAssertion(assert.ValueSet("directory.0.enable", strconv.FormatBool(opts.Enable)))
-	e.AddAssertion(assert.ValueSet("directory.0.auto_refresh", autoRefresh))
-	e.AddAssertion(assert.ValueSet("directory.0.notification_integration", notificationIntegration))
-	e.AddAssertion(assert.ValueSet("directory.0.refresh_on_create", refreshOnCreate))
+	e.ValueSet("directory.#", "1")
+	e.ValueSet("directory.0.enable", strconv.FormatBool(opts.Enable))
+	e.ValueSet("directory.0.auto_refresh", autoRefresh)
+	e.ValueSet("directory.0.notification_integration", notificationIntegration)
+	e.ValueSet("directory.0.refresh_on_create", refreshOnCreate)
 	return e
 }
 
 func (e *ExternalAzureStageResourceAssert) HasEncryptionAzureCse() *ExternalAzureStageResourceAssert {
-	e.AddAssertion(assert.ValueSet("encryption.#", "1"))
-	e.AddAssertion(assert.ValueSet("encryption.0.azure_cse.#", "1"))
-	e.AddAssertion(assert.ValueSet("encryption.0.none.#", "0"))
+	e.ValueSet("encryption.#", "1")
+	e.ValueSet("encryption.0.azure_cse.#", "1")
+	e.ValueSet("encryption.0.none.#", "0")
 	return e
 }
 
 func (e *ExternalAzureStageResourceAssert) HasEncryptionNone() *ExternalAzureStageResourceAssert {
-	e.AddAssertion(assert.ValueSet("encryption.#", "1"))
-	e.AddAssertion(assert.ValueSet("encryption.0.azure_cse.#", "0"))
-	e.AddAssertion(assert.ValueSet("encryption.0.none.#", "1"))
+	e.ValueSet("encryption.#", "1")
+	e.ValueSet("encryption.0.azure_cse.#", "0")
+	e.ValueSet("encryption.0.none.#", "1")
 	return e
 }
 
 func (e *ExternalAzureStageResourceAssert) HasStageTypeEnum(expected sdk.StageType) *ExternalAzureStageResourceAssert {
-	e.AddAssertion(assert.ValueSet("stage_type", string(expected)))
+	e.ValueSet("stage_type", string(expected))
 	return e
 }
 
 func (e *ExternalAzureStageResourceAssert) HasCloudEnum(expected sdk.StageCloud) *ExternalAzureStageResourceAssert {
-	e.AddAssertion(assert.ValueSet("cloud", string(expected)))
+	e.ValueSet("cloud", string(expected))
 	return e
 }
 
 func (e *ExternalAzureStageResourceAssert) HasFileFormatFormatName(expected string) *ExternalAzureStageResourceAssert {
-	for _, a := range stageHasFileFormatFormatName(expected) {
-		e.AddAssertion(a)
-	}
+	stageApplyFileFormatFormatNameChecks(e.ResourceAssert, expected)
 	return e
 }
 
 func (e *ExternalAzureStageResourceAssert) HasFileFormatCsv() *ExternalAzureStageResourceAssert {
-	for _, a := range stageHasFileFormatCsv() {
-		e.AddAssertion(a)
-	}
+	stageApplyFileFormatCsvChecks(e.ResourceAssert)
 	return e
 }
 
 func (e *ExternalAzureStageResourceAssert) HasCredentials(token string) *ExternalAzureStageResourceAssert {
-	e.AddAssertion(assert.ValueSet("credentials.#", "1"))
-	e.AddAssertion(assert.ValueSet("credentials.0.azure_sas_token", token))
+	e.ValueSet("credentials.#", "1")
+	e.ValueSet("credentials.0.azure_sas_token", token)
 	return e
 }
--- pkg/acceptance/bettertestspoc/assert/resourceassert/stage_common.go
+++ pkg/acceptance/bettertestspoc/assert/resourceassert/stage_common.go
@@ -27,3 +27,25 @@ func stageHasFileFormatCsv() []assert.ResourceAssertion {
 		assert.ValueSet("file_format.0.xml.#", "0"),
 	}
 }
+
+func stageApplyFileFormatFormatNameChecks(e *assert.ResourceAssert, expected string) {
+	e.ValueSet("file_format.#", "1")
+	e.ValueSet("file_format.0.format_name", expected)
+	e.ValueSet("file_format.0.csv.#", "0")
+	e.ValueSet("file_format.0.json.#", "0")
+	e.ValueSet("file_format.0.avro.#", "0")
+	e.ValueSet("file_format.0.orc.#", "0")
+	e.ValueSet("file_format.0.parquet.#", "0")
+	e.ValueSet("file_format.0.xml.#", "0")
+}
+
+func stageApplyFileFormatCsvChecks(e *assert.ResourceAssert) {
+	e.ValueSet("file_format.#", "1")
+	e.ValueSet("file_format.0.csv.#", "1")
+	e.ValueSet("file_format.0.format_name", "")
+	e.ValueSet("file_format.0.json.#", "0")
+	e.ValueSet("file_format.0.avro.#", "0")
+	e.ValueSet("file_format.0.orc.#", "0")
+	e.ValueSet("file_format.0.parquet.#", "0")
+	e.ValueSet("file_format.0.xml.#", "0")
+}
