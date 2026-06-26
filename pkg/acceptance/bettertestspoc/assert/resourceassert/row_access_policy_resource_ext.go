437af86eda97ed94b9fe7af4d06a6558aa5825f1
Artur Sawicki <artur.sawicki@snowflake.com>
2026-06-26 16:15:12 +0200

    Regenerate row access policy resource assertions

--- pkg/acceptance/bettertestspoc/assert/resourceassert/row_access_policy_resource_ext.go
+++ pkg/acceptance/bettertestspoc/assert/resourceassert/row_access_policy_resource_ext.go
@@ -4,15 +4,14 @@ import (
 	"fmt"
 	"strconv"
 
-	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/acceptance/bettertestspoc/assert"
 	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/sdk"
 )
 
 func (r *RowAccessPolicyResourceAssert) HasArguments(args []sdk.TableColumnSignature) *RowAccessPolicyResourceAssert {
-	r.AddAssertion(assert.ValueSet("argument.#", strconv.FormatInt(int64(len(args)), 10)))
+	r.ValueSet("argument.#", strconv.FormatInt(int64(len(args)), 10))
 	for i, v := range args {
-		r.AddAssertion(assert.ValueSet(fmt.Sprintf("argument.%d.name", i), v.Name))
-		r.AddAssertion(assert.ValueSet(fmt.Sprintf("argument.%d.type", i), v.Type.ToSql()))
+		r.ValueSet(fmt.Sprintf("argument.%d.name", i), v.Name)
+		r.ValueSet(fmt.Sprintf("argument.%d.type", i), v.Type.ToSql())
 	}
 	return r
 }
