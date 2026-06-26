d786c04317e9319bac707757899b1820614b9a4b
Artur Sawicki <artur.sawicki@snowflake.com>
2026-06-26 15:05:35 +0200

    Regenerate external s3 stage resource assertions

--- pkg/acceptance/bettertestspoc/assert/resourceassert/external_s3_stage_resource_ext.go
+++ pkg/acceptance/bettertestspoc/assert/resourceassert/external_s3_stage_resource_ext.go
@@ -3,7 +3,6 @@ package resourceassert
 import (
 	"strconv"
 
-	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/acceptance/bettertestspoc/assert"
 	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/sdk"
 )
 
@@ -24,82 +23,78 @@ func (e *ExternalS3StageResourceAssert) HasDirectory(opts ExternalS3StageDirecto
 	if opts.AutoRefresh != nil {
 		autoRefresh = *opts.AutoRefresh
 	}
-	e.AddAssertion(assert.ValueSet("directory.#", "1"))
-	e.AddAssertion(assert.ValueSet("directory.0.enable", strconv.FormatBool(opts.Enable)))
-	e.AddAssertion(assert.ValueSet("directory.0.auto_refresh", autoRefresh))
-	e.AddAssertion(assert.ValueSet("directory.0.refresh_on_create", refreshOnCreate))
+	e.ValueSet("directory.#", "1")
+	e.ValueSet("directory.0.enable", strconv.FormatBool(opts.Enable))
+	e.ValueSet("directory.0.auto_refresh", autoRefresh)
+	e.ValueSet("directory.0.refresh_on_create", refreshOnCreate)
 	return e
 }
 
 func (e *ExternalS3StageResourceAssert) HasEncryptionAwsCse() *ExternalS3StageResourceAssert {
-	e.AddAssertion(assert.ValueSet("encryption.#", "1"))
-	e.AddAssertion(assert.ValueSet("encryption.0.aws_cse.#", "1"))
-	e.AddAssertion(assert.ValueSet("encryption.0.aws_sse_s3.#", "0"))
-	e.AddAssertion(assert.ValueSet("encryption.0.aws_sse_kms.#", "0"))
-	e.AddAssertion(assert.ValueSet("encryption.0.none.#", "0"))
+	e.ValueSet("encryption.#", "1")
+	e.ValueSet("encryption.0.aws_cse.#", "1")
+	e.ValueSet("encryption.0.aws_sse_s3.#", "0")
+	e.ValueSet("encryption.0.aws_sse_kms.#", "0")
+	e.ValueSet("encryption.0.none.#", "0")
 	return e
 }
 
 func (e *ExternalS3StageResourceAssert) HasEncryptionAwsSseS3() *ExternalS3StageResourceAssert {
-	e.AddAssertion(assert.ValueSet("encryption.#", "1"))
-	e.AddAssertion(assert.ValueSet("encryption.0.aws_cse.#", "0"))
-	e.AddAssertion(assert.ValueSet("encryption.0.aws_sse_s3.#", "1"))
-	e.AddAssertion(assert.ValueSet("encryption.0.aws_sse_kms.#", "0"))
-	e.AddAssertion(assert.ValueSet("encryption.0.none.#", "0"))
+	e.ValueSet("encryption.#", "1")
+	e.ValueSet("encryption.0.aws_cse.#", "0")
+	e.ValueSet("encryption.0.aws_sse_s3.#", "1")
+	e.ValueSet("encryption.0.aws_sse_kms.#", "0")
+	e.ValueSet("encryption.0.none.#", "0")
 	return e
 }
 
 func (e *ExternalS3StageResourceAssert) HasEncryptionAwsSseKms() *ExternalS3StageResourceAssert {
-	e.AddAssertion(assert.ValueSet("encryption.#", "1"))
-	e.AddAssertion(assert.ValueSet("encryption.0.aws_cse.#", "0"))
-	e.AddAssertion(assert.ValueSet("encryption.0.aws_sse_s3.#", "0"))
-	e.AddAssertion(assert.ValueSet("encryption.0.aws_sse_kms.#", "1"))
-	e.AddAssertion(assert.ValueSet("encryption.0.none.#", "0"))
+	e.ValueSet("encryption.#", "1")
+	e.ValueSet("encryption.0.aws_cse.#", "0")
+	e.ValueSet("encryption.0.aws_sse_s3.#", "0")
+	e.ValueSet("encryption.0.aws_sse_kms.#", "1")
+	e.ValueSet("encryption.0.none.#", "0")
 	return e
 }
 
 func (e *ExternalS3StageResourceAssert) HasEncryptionNone() *ExternalS3StageResourceAssert {
-	e.AddAssertion(assert.ValueSet("encryption.#", "1"))
-	e.AddAssertion(assert.ValueSet("encryption.0.aws_cse.#", "0"))
-	e.AddAssertion(assert.ValueSet("encryption.0.aws_sse_s3.#", "0"))
-	e.AddAssertion(assert.ValueSet("encryption.0.aws_sse_kms.#", "0"))
-	e.AddAssertion(assert.ValueSet("encryption.0.none.#", "1"))
+	e.ValueSet("encryption.#", "1")
+	e.ValueSet("encryption.0.aws_cse.#", "0")
+	e.ValueSet("encryption.0.aws_sse_s3.#", "0")
+	e.ValueSet("encryption.0.aws_sse_kms.#", "0")
+	e.ValueSet("encryption.0.none.#", "1")
 	return e
 }
 
 func (e *ExternalS3StageResourceAssert) HasStageTypeEnum(expected sdk.StageType) *ExternalS3StageResourceAssert {
-	e.AddAssertion(assert.ValueSet("stage_type", string(expected)))
+	e.ValueSet("stage_type", string(expected))
 	return e
 }
 
 func (e *ExternalS3StageResourceAssert) HasCloudEnum(expected sdk.StageCloud) *ExternalS3StageResourceAssert {
-	e.AddAssertion(assert.ValueSet("cloud", string(expected)))
+	e.ValueSet("cloud", string(expected))
 	return e
 }
 
 func (e *ExternalS3StageResourceAssert) HasFileFormatFormatName(expected string) *ExternalS3StageResourceAssert {
-	for _, a := range stageHasFileFormatFormatName(expected) {
-		e.AddAssertion(a)
-	}
+	stageApplyFileFormatFormatNameChecks(e.ResourceAssert, expected)
 	return e
 }
 
 func (e *ExternalS3StageResourceAssert) HasFileFormatCsv() *ExternalS3StageResourceAssert {
-	for _, a := range stageHasFileFormatCsv() {
-		e.AddAssertion(a)
-	}
+	stageApplyFileFormatCsvChecks(e.ResourceAssert)
 	return e
 }
 
 func (e *ExternalS3StageResourceAssert) HasCredentialsAwsKey(keyId, secretKey string) *ExternalS3StageResourceAssert {
-	e.AddAssertion(assert.ValueSet("credentials.#", "1"))
-	e.AddAssertion(assert.ValueSet("credentials.0.aws_key_id", keyId))
-	e.AddAssertion(assert.ValueSet("credentials.0.aws_secret_key", secretKey))
+	e.ValueSet("credentials.#", "1")
+	e.ValueSet("credentials.0.aws_key_id", keyId)
+	e.ValueSet("credentials.0.aws_secret_key", secretKey)
 	return e
 }
 
 func (e *ExternalS3StageResourceAssert) HasCredentialsAwsRole(roleArn string) *ExternalS3StageResourceAssert {
-	e.AddAssertion(assert.ValueSet("credentials.#", "1"))
-	e.AddAssertion(assert.ValueSet("credentials.0.aws_role", roleArn))
+	e.ValueSet("credentials.#", "1")
+	e.ValueSet("credentials.0.aws_role", roleArn)
 	return e
 }
