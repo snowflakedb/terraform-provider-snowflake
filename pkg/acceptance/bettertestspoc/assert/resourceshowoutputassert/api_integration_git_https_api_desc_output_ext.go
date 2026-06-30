47b530d4566f107ee1256ad0cfd12ce57288c952
Artur Sawicki <artur.sawicki@snowflake.com>
2026-06-30 15:14:34 +0200

    Regenerate api integration git https api resource describe output assertions

--- pkg/acceptance/bettertestspoc/assert/resourceshowoutputassert/api_integration_git_https_api_desc_output_ext.go
+++ pkg/acceptance/bettertestspoc/assert/resourceshowoutputassert/api_integration_git_https_api_desc_output_ext.go
@@ -3,30 +3,28 @@ package resourceshowoutputassert
 import (
 	"fmt"
 	"strconv"
-
-	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/acceptance/bettertestspoc/assert"
 )
 
 func (a *ApiIntegrationGitHttpsApiDescribeOutputAssert) HasOauthAllowedScopes(expected ...string) *ApiIntegrationGitHttpsApiDescribeOutputAssert {
-	a.AddAssertion(assert.ResourceDescribeOutputValueSet("oauth_allowed_scopes.#", strconv.Itoa(len(expected))))
+	a.StringValueSet("oauth_allowed_scopes.#", strconv.Itoa(len(expected)))
 	for i, scope := range expected {
-		a.AddAssertion(assert.ResourceDescribeOutputValueSet(fmt.Sprintf("oauth_allowed_scopes.%d", i), scope))
+		a.StringValueSet(fmt.Sprintf("oauth_allowed_scopes.%d", i), scope)
 	}
 	return a
 }
 
 func (a *ApiIntegrationGitHttpsApiDescribeOutputAssert) HasAllowedPrefixes(expected ...string) *ApiIntegrationGitHttpsApiDescribeOutputAssert {
-	a.AddAssertion(assert.ResourceDescribeOutputValueSet("allowed_prefixes.#", strconv.Itoa(len(expected))))
+	a.StringValueSet("allowed_prefixes.#", strconv.Itoa(len(expected)))
 	for i, prefix := range expected {
-		a.AddAssertion(assert.ResourceDescribeOutputValueSet(fmt.Sprintf("allowed_prefixes.%d", i), prefix))
+		a.StringValueSet(fmt.Sprintf("allowed_prefixes.%d", i), prefix)
 	}
 	return a
 }
 
 func (a *ApiIntegrationGitHttpsApiDescribeOutputAssert) HasBlockedPrefixes(expected ...string) *ApiIntegrationGitHttpsApiDescribeOutputAssert {
-	a.AddAssertion(assert.ResourceDescribeOutputValueSet("blocked_prefixes.#", strconv.Itoa(len(expected))))
+	a.StringValueSet("blocked_prefixes.#", strconv.Itoa(len(expected)))
 	for i, prefix := range expected {
-		a.AddAssertion(assert.ResourceDescribeOutputValueSet(fmt.Sprintf("blocked_prefixes.%d", i), prefix))
+		a.StringValueSet(fmt.Sprintf("blocked_prefixes.%d", i), prefix)
 	}
 	return a
 }
