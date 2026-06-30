7cf442ed3b15aca81cc488905d449febb9a8c848
Artur Sawicki <artur.sawicki@snowflake.com>
2026-06-30 13:33:10 +0200

    Regenerate row access policy resource show output assertions

--- pkg/acceptance/bettertestspoc/assert/resourceshowoutputassert/row_access_policy_show_output_ext.go
+++ pkg/acceptance/bettertestspoc/assert/resourceshowoutputassert/row_access_policy_show_output_ext.go
@@ -1,23 +1,6 @@
 package resourceshowoutputassert
 
-import (
-	"testing"
-
-	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/acceptance/bettertestspoc/assert"
-)
-
 func (r *RowAccessPolicyShowOutputAssert) HasCreatedOnNotEmpty() *RowAccessPolicyShowOutputAssert {
-	r.AddAssertion(assert.ResourceShowOutputValuePresent("created_on"))
+	r.ValuePresent("created_on")
 	return r
-}
-
-// RowAccessPoliciesDatasourceShowOutput is a temporary workaround to have better show output assertions in data source acceptance tests.
-func RowAccessPoliciesDatasourceShowOutput(t *testing.T, datasourceReference string) *RowAccessPolicyShowOutputAssert {
-	t.Helper()
-
-	r := RowAccessPolicyShowOutputAssert{
-		ResourceAssert: assert.NewDatasourceAssert(datasourceReference, "show_output", "row_access_policies.0."),
-	}
-	r.AddAssertion(assert.ValueSet("show_output.#", "1"))
-	return &r
 }
