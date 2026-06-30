98f63d6463fd5c0032975b270aa1d445fbb8322a
Artur Sawicki <artur.sawicki@snowflake.com>
2026-06-30 13:40:18 +0200

    Regenerate secret resource show output assertions

--- pkg/acceptance/bettertestspoc/assert/resourceshowoutputassert/secret_show_output_ext.go
+++ pkg/acceptance/bettertestspoc/assert/resourceshowoutputassert/secret_show_output_ext.go
@@ -1,10 +1,6 @@
 package resourceshowoutputassert
 
-import (
-	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/acceptance/bettertestspoc/assert"
-)
-
 func (s *SecretShowOutputAssert) HasCreatedOnNotEmpty() *SecretShowOutputAssert {
-	s.AddAssertion(assert.ResourceShowOutputValuePresent("created_on"))
+	s.ValuePresent("created_on")
 	return s
 }
