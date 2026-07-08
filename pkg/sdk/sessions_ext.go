d166af70b67648f27deac22766f13d12c6ce1abd
Artur Sawicki <artur.sawicki@snowflake.com>
2026-07-08 18:01:32 +0200

    Adjust the callers

--- pkg/sdk/sessions_ext.go
+++ pkg/sdk/sessions_ext.go
@@ -1,40 +1,7 @@
 package sdk
 
-import (
-	"context"
-	"fmt"
-)
+import "context"
 
 func (v *sessions) ShowParameters(ctx context.Context, opts *ShowParametersOptions) ([]*Parameter, error) {
 	return v.client.Parameters.ShowParameters(ctx, opts)
-}
-
-func (v *sessions) UseWarehouse(ctx context.Context, warehouse AccountObjectIdentifier) error {
-	sql := fmt.Sprintf(`USE WAREHOUSE %s`, warehouse.FullyQualifiedName())
-	_, err := v.client.exec(ctx, sql)
-	return err
-}
-
-func (v *sessions) UseDatabase(ctx context.Context, database AccountObjectIdentifier) error {
-	sql := fmt.Sprintf(`USE DATABASE %s`, database.FullyQualifiedName())
-	_, err := v.client.exec(ctx, sql)
-	return err
-}
-
-func (v *sessions) UseSchema(ctx context.Context, schema DatabaseObjectIdentifier) error {
-	sql := fmt.Sprintf(`USE SCHEMA %s`, schema.FullyQualifiedName())
-	_, err := v.client.exec(ctx, sql)
-	return err
-}
-
-func (v *sessions) UseRole(ctx context.Context, role AccountObjectIdentifier) error {
-	sql := fmt.Sprintf(`USE ROLE %s`, role.FullyQualifiedName())
-	_, err := v.client.exec(ctx, sql)
-	return err
-}
-
-func (v *sessions) UseSecondaryRoles(ctx context.Context, opt SecondaryRoleOption) error {
-	sql := fmt.Sprintf(`USE SECONDARY ROLES %s`, opt)
-	_, err := v.client.exec(ctx, sql)
-	return err
 }
