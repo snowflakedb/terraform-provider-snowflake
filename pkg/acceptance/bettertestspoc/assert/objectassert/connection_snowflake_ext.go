e968f97851ec6b9daff490a0486146840541efae
Artur Sawicki <artur.sawicki@snowflake.com>
2026-07-20 17:06:58 +0200

    Generate for connection

--- pkg/acceptance/bettertestspoc/assert/objectassert/connection_snowflake_ext.go
+++ pkg/acceptance/bettertestspoc/assert/objectassert/connection_snowflake_ext.go
@@ -7,17 +7,6 @@ import (
 	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/sdk"
 )
 
-func (c *ConnectionAssert) HasNoComment() *ConnectionAssert {
-	c.AddAssertion(func(t *testing.T, o *sdk.Connection) error {
-		t.Helper()
-		if o.Comment != nil {
-			return fmt.Errorf("expected comment to have nil; got: %s", *o.Comment)
-		}
-		return nil
-	})
-	return c
-}
-
 func (c *ConnectionAssert) HasConnectionUrlNotEmpty() *ConnectionAssert {
 	c.AddAssertion(func(t *testing.T, o *sdk.Connection) error {
 		t.Helper()
