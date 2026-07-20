cda047c5c826a0bb5797892a4f49af261af9c62d
Artur Sawicki <artur.sawicki@snowflake.com>
2026-07-20 17:01:40 +0200

    Generate for organization account

--- pkg/acceptance/bettertestspoc/assert/objectassert/organization_account_snowflake_gen.go
+++ pkg/acceptance/bettertestspoc/assert/objectassert/organization_account_snowflake_gen.go
@@ -111,6 +111,17 @@ func (o *OrganizationAccountAssert) HasComment(expected string) *OrganizationAcc
 	return o
 }
 
+func (o *OrganizationAccountAssert) HasNoComment() *OrganizationAccountAssert {
+	o.AddAssertion(func(t *testing.T, o *sdk.OrganizationAccount) error {
+		t.Helper()
+		if o.Comment != nil {
+			return fmt.Errorf("expected comment to be nil; got: %v", *o.Comment)
+		}
+		return nil
+	})
+	return o
+}
+
 func (o *OrganizationAccountAssert) HasAccountLocator(expected string) *OrganizationAccountAssert {
 	o.AddAssertion(func(t *testing.T, o *sdk.OrganizationAccount) error {
 		t.Helper()
@@ -169,6 +180,17 @@ func (o *OrganizationAccountAssert) HasMarketplaceConsumerBillingEntityName(expe
 	return o
 }
 
+func (o *OrganizationAccountAssert) HasNoMarketplaceConsumerBillingEntityName() *OrganizationAccountAssert {
+	o.AddAssertion(func(t *testing.T, o *sdk.OrganizationAccount) error {
+		t.Helper()
+		if o.MarketplaceConsumerBillingEntityName != nil {
+			return fmt.Errorf("expected marketplace consumer billing entity name to be nil; got: %v", *o.MarketplaceConsumerBillingEntityName)
+		}
+		return nil
+	})
+	return o
+}
+
 func (o *OrganizationAccountAssert) HasMarketplaceProviderBillingEntityName(expected string) *OrganizationAccountAssert {
 	o.AddAssertion(func(t *testing.T, o *sdk.OrganizationAccount) error {
 		t.Helper()
@@ -183,6 +205,17 @@ func (o *OrganizationAccountAssert) HasMarketplaceProviderBillingEntityName(expe
 	return o
 }
 
+func (o *OrganizationAccountAssert) HasNoMarketplaceProviderBillingEntityName() *OrganizationAccountAssert {
+	o.AddAssertion(func(t *testing.T, o *sdk.OrganizationAccount) error {
+		t.Helper()
+		if o.MarketplaceProviderBillingEntityName != nil {
+			return fmt.Errorf("expected marketplace provider billing entity name to be nil; got: %v", *o.MarketplaceProviderBillingEntityName)
+		}
+		return nil
+	})
+	return o
+}
+
 func (o *OrganizationAccountAssert) HasOldAccountUrl(expected string) *OrganizationAccountAssert {
 	o.AddAssertion(func(t *testing.T, o *sdk.OrganizationAccount) error {
 		t.Helper()
@@ -191,6 +224,17 @@ func (o *OrganizationAccountAssert) HasOldAccountUrl(expected string) *Organizat
 		}
 		if *o.OldAccountUrl != expected {
 			return fmt.Errorf("expected old account url: %v; got: %v", expected, *o.OldAccountUrl)
+		}
+		return nil
+	})
+	return o
+}
+
+func (o *OrganizationAccountAssert) HasNoOldAccountUrl() *OrganizationAccountAssert {
+	o.AddAssertion(func(t *testing.T, o *sdk.OrganizationAccount) error {
+		t.Helper()
+		if o.OldAccountUrl != nil {
+			return fmt.Errorf("expected old account url to be nil; got: %v", *o.OldAccountUrl)
 		}
 		return nil
 	})
@@ -222,6 +266,17 @@ func (o *OrganizationAccountAssert) HasAccountOldUrlSavedOn(expected string) *Or
 	return o
 }
 
+func (o *OrganizationAccountAssert) HasNoAccountOldUrlSavedOn() *OrganizationAccountAssert {
+	o.AddAssertion(func(t *testing.T, o *sdk.OrganizationAccount) error {
+		t.Helper()
+		if o.AccountOldUrlSavedOn != nil {
+			return fmt.Errorf("expected account old url saved on to be nil; got: %v", *o.AccountOldUrlSavedOn)
+		}
+		return nil
+	})
+	return o
+}
+
 func (o *OrganizationAccountAssert) HasAccountOldUrlLastUsed(expected string) *OrganizationAccountAssert {
 	o.AddAssertion(func(t *testing.T, o *sdk.OrganizationAccount) error {
 		t.Helper()
@@ -230,6 +285,17 @@ func (o *OrganizationAccountAssert) HasAccountOldUrlLastUsed(expected string) *O
 		}
 		if *o.AccountOldUrlLastUsed != expected {
 			return fmt.Errorf("expected account old url last used: %v; got: %v", expected, *o.AccountOldUrlLastUsed)
+		}
+		return nil
+	})
+	return o
+}
+
+func (o *OrganizationAccountAssert) HasNoAccountOldUrlLastUsed() *OrganizationAccountAssert {
+	o.AddAssertion(func(t *testing.T, o *sdk.OrganizationAccount) error {
+		t.Helper()
+		if o.AccountOldUrlLastUsed != nil {
+			return fmt.Errorf("expected account old url last used to be nil; got: %v", *o.AccountOldUrlLastUsed)
 		}
 		return nil
 	})
@@ -250,6 +316,17 @@ func (o *OrganizationAccountAssert) HasOrganizationOldUrl(expected string) *Orga
 	return o
 }
 
+func (o *OrganizationAccountAssert) HasNoOrganizationOldUrl() *OrganizationAccountAssert {
+	o.AddAssertion(func(t *testing.T, o *sdk.OrganizationAccount) error {
+		t.Helper()
+		if o.OrganizationOldUrl != nil {
+			return fmt.Errorf("expected organization old url to be nil; got: %v", *o.OrganizationOldUrl)
+		}
+		return nil
+	})
+	return o
+}
+
 func (o *OrganizationAccountAssert) HasOrganizationOldUrlSavedOn(expected string) *OrganizationAccountAssert {
 	o.AddAssertion(func(t *testing.T, o *sdk.OrganizationAccount) error {
 		t.Helper()
@@ -264,6 +341,17 @@ func (o *OrganizationAccountAssert) HasOrganizationOldUrlSavedOn(expected string
 	return o
 }
 
+func (o *OrganizationAccountAssert) HasNoOrganizationOldUrlSavedOn() *OrganizationAccountAssert {
+	o.AddAssertion(func(t *testing.T, o *sdk.OrganizationAccount) error {
+		t.Helper()
+		if o.OrganizationOldUrlSavedOn != nil {
+			return fmt.Errorf("expected organization old url saved on to be nil; got: %v", *o.OrganizationOldUrlSavedOn)
+		}
+		return nil
+	})
+	return o
+}
+
 func (o *OrganizationAccountAssert) HasOrganizationOldUrlLastUsed(expected string) *OrganizationAccountAssert {
 	o.AddAssertion(func(t *testing.T, o *sdk.OrganizationAccount) error {
 		t.Helper()
@@ -272,6 +360,17 @@ func (o *OrganizationAccountAssert) HasOrganizationOldUrlLastUsed(expected strin
 		}
 		if *o.OrganizationOldUrlLastUsed != expected {
 			return fmt.Errorf("expected organization old url last used: %v; got: %v", expected, *o.OrganizationOldUrlLastUsed)
+		}
+		return nil
+	})
+	return o
+}
+
+func (o *OrganizationAccountAssert) HasNoOrganizationOldUrlLastUsed() *OrganizationAccountAssert {
+	o.AddAssertion(func(t *testing.T, o *sdk.OrganizationAccount) error {
+		t.Helper()
+		if o.OrganizationOldUrlLastUsed != nil {
+			return fmt.Errorf("expected organization old url last used to be nil; got: %v", *o.OrganizationOldUrlLastUsed)
 		}
 		return nil
 	})
